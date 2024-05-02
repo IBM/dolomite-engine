9702b21bd12926c7aaa2c1a7ba48a1370e9afeb8
Mayank Mishra <Mayank.Mishra2@ibm.com>
2024-05-02 02:22:17 -0400

    compile (#69)

--- dolomite_engine/hf_models/modeling_utils/activations/math_gelu.py
+++ dolomite_engine/hf_models/modeling_utils/activations/math_gelu.py
@@ -1,12 +1,12 @@
 import torch
 
 
-@torch.jit.script
+@torch.compile
 def _gelu_forward(x: torch.Tensor) -> torch.Tensor:
     return x * 0.5 * (1.0 + torch.tanh(0.79788456 * x * (1 + 0.044715 * x * x)))
 
 
-@torch.jit.script
+@torch.compile
 def _gelu_backward(gradient: torch.Tensor, x: torch.Tensor) -> torch.Tensor:
     tanh_out = torch.tanh(0.79788456 * x * (1 + 0.044715 * x * x))
     ff = 0.5 * x * ((1 - tanh_out * tanh_out) * (0.79788456 + 0.1070322243 * x * x)) + 0.5 * (1 + tanh_out)
