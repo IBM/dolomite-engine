cae3898647b5d8c3f7dac91d9bef91a8c01ee292
Mayank Mishra <32954280+mayank31398@users.noreply.github.com>
2024-06-10 14:20:26 -0400

    add reentrant (#32)

--- dolomite_engine/gradient_checkpointing/block.py
+++ dolomite_engine/gradient_checkpointing/block.py
@@ -1,10 +1,18 @@
+from functools import partial
+
 import torch.nn as nn
-from torch.distributed.algorithms._checkpoint.checkpoint_wrapper import apply_activation_checkpointing
+from torch.distributed.algorithms._checkpoint.checkpoint_wrapper import (
+    CheckpointImpl,
+    apply_activation_checkpointing,
+    checkpoint_wrapper,
+)
 
 from ..utils import get_module_class_from_name
 
 
-def block_checkpointing(model: nn.Module, block_name: str, checkpoint_every: int = 1) -> None:
+def block_checkpointing(
+    model: nn.Module, block_name: str, checkpoint_every: int = 1, use_reentrant: bool = False
+) -> None:
     block_class = get_module_class_from_name(model, block_name)
     block_idx = 0
 
@@ -17,4 +25,10 @@ def block_checkpointing(model: nn.Module, block_name: str, checkpoint_every: int
                 return True
         return False
 
-    apply_activation_checkpointing(model, check_fn=_whether_to_checkpoint)
+    checkpoint_wrapper_function = checkpoint_wrapper
+    if use_reentrant:
+        checkpoint_wrapper_function = partial(checkpoint_wrapper, checkpoint_impl=CheckpointImpl.REENTRANT)
+
+    apply_activation_checkpointing(
+        model, checkpoint_wrapper_fn=checkpoint_wrapper_function, check_fn=_whether_to_checkpoint
+    )
