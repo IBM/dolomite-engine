49a56a1654a21893c71e3dcd4b84c22e6bdf0037
Mayank Mishra <mayank31398@gmail.com>
2024-06-21 22:49:31 -0400

    stash

---
+++ dolomite_engine/hf_models/models/gpt_ensemble/__init__.py
@@ -0,0 +1,3 @@
+from .base import GPTDolomiteModel, GPTDolomitePreTrainedModel
+from .config import GPTEnsembleConfig
+from .main import GPTDolomiteForCausalLM
---
+++ dolomite_engine/hf_models/models/gpt_ensemble/base.py
@@ -0,0 +1,162 @@
+from typing import Tuple, Union
+
+import torch
+import torch.nn as nn
+from transformers import DynamicCache
+from transformers.modeling_outputs import BaseModelOutputWithPast
+
+from ...enums import AttentionHeadType, PositionEmbeddingType
+from ...modeling_utils import Alibi, ParameterizedEmbedding, RoPE, YaRNScaledRoPE, get_normalization_function
+from ...utils import divide_if_divisible
+from ..gpt_dolomite import GPTDolomitePreTrainedModel
+from .config import GPTEnsembleConfig
+from .layer import GPTEnsembleBlock
+
+
+class GPTEnsemblePreTrainedModel(GPTDolomitePreTrainedModel):
+    config_class = GPTEnsembleConfig
+    _no_split_modules = ["GPTEnsembleBlock"]
+
+
+class GPTEnsembleModel(GPTEnsemblePreTrainedModel):
+    def __init__(self, config: GPTEnsembleConfig, **kwargs) -> None:
+        super().__init__(config, **kwargs)
+
+        self.attention_head_type = AttentionHeadType(config.attention_head_type)
+        self.embed_dim = config.hidden_size
+        self.num_heads = config.num_attention_heads
+        self.m_emb = config.m_emb
+        self.initializer_range = config.initializer_range
+
+        self.head_dim = divide_if_divisible(
+            self.embed_dim,
+            self.num_heads,
+            f"`embed_dim` ({self.embed_dim}) must be divisible by `num_heads` ({self.num_heads})",
+        )
+
+        self.wte = ParameterizedEmbedding(config.vocab_size, self.embed_dim, std=self.initializer_range)
+
+        self.drop = nn.Identity() if config.embd_pdrop == 0 else nn.Dropout(config.embd_pdrop)
+        self.h = nn.ModuleList(
+            [
+                GPTEnsembleBlock(
+                    config,
+                    self.normalization_implementation,
+                    self.attention_implementation,
+                    self._use_padding_free_transformer,
+                    layer_idx=i,
+                )
+                for i in range(config.num_hidden_layers)
+            ]
+        )
+        self.ln_f = get_normalization_function(
+            config.normalization_function,
+            self.embed_dim,
+            eps=config.layer_norm_epsilon,
+            normalization_implementation=self.normalization_implementation,
+            elementwise_affine=False,
+        )
+
+        self.position_embedding_type = PositionEmbeddingType(config.position_embedding_type)
+        self._setup_positional_encoding()
+
+        # Initialize weights and apply final processing
+        self.post_init()
+
+    def forward(
+        self,
+        input_ids: torch.Tensor = None,
+        past_key_values: DynamicCache = None,
+        attention_mask: torch.Tensor = None,
+        token_type_ids: torch.Tensor = None,
+        position_ids: torch.Tensor = None,
+        inputs_embeds: torch.Tensor = None,
+        use_cache: bool = None,
+        output_hidden_states: bool = None,
+        return_dict: bool = None,
+        cu_seqlens: torch.Tensor = None,
+        max_seqlen: torch.Tensor = None,
+    ) -> Union[Tuple, BaseModelOutputWithPast]:
+        (
+            output_hidden_states,
+            use_cache,
+            return_dict,
+            input_shape,
+            hidden_states,
+            attention_mask,
+            position_ids,
+            rope_cos_sin,
+            past_key_values,
+        ) = self._prepare_a_bunch_of_stuff(
+            input_ids=input_ids,
+            past_key_values=past_key_values,
+            attention_mask=attention_mask,
+            token_type_ids=token_type_ids,
+            position_ids=position_ids,
+            inputs_embeds=inputs_embeds,
+            use_cache=use_cache,
+            output_hidden_states=output_hidden_states,
+            return_dict=return_dict,
+            cu_seqlens=cu_seqlens,
+            max_seqlen=max_seqlen,
+        )
+
+        output_shape = input_shape + (hidden_states.size(-1),)
+
+        past_key_values = DynamicCache() if use_cache and past_key_values is None else past_key_values
+        all_hidden_states = () if output_hidden_states else None
+        for block in self.h:
+            if output_hidden_states:
+                all_hidden_states += (hidden_states,)
+
+            hidden_states = block(
+                hidden_states,
+                past_key_values=past_key_values,
+                attention_mask=attention_mask,
+                rope_cos_sin=rope_cos_sin,
+                cu_seqlens=cu_seqlens,
+                max_seqlen=max_seqlen,
+            )
+
+        hidden_states = self.ln_f(hidden_states)
+
+        hidden_states = hidden_states.view(output_shape)
+        # Add last hidden state
+        if output_hidden_states:
+            all_hidden_states += (hidden_states,)
+
+        if not return_dict:
+            return tuple(v for v in [hidden_states, past_key_values, all_hidden_states] if v is not None)
+
+        return BaseModelOutputWithPast(
+            last_hidden_state=hidden_states,
+            past_key_values=past_key_values,
+            hidden_states=all_hidden_states,
+        )
+
+    def _setup_positional_encoding(self) -> None:
+        max_position_embeddings = self.config.max_position_embeddings
+
+        if self.position_embedding_type == PositionEmbeddingType.learned_absolute:
+            self.wpe = ParameterizedEmbedding(max_position_embeddings, self.embed_dim, std=self.initializer_range)
+        elif self.position_embedding_type == PositionEmbeddingType.alibi:
+            assert not self._use_flash_attention_2, "alibi is not implemented with FlashAttention"
+
+            self.alibi = Alibi(self.num_heads)
+        elif self.position_embedding_type == PositionEmbeddingType.rope:
+            if self.config.rope_scaling is None:
+                self.rope = RoPE(
+                    self.head_dim,
+                    max_position_embeddings=max_position_embeddings,
+                    base=self.config.rope_theta,
+                )
+            else:
+                self.rope = YaRNScaledRoPE(
+                    self.head_dim,
+                    max_position_embeddings=max_position_embeddings,
+                    base=self.config.rope_theta,
+                    scale=self.config.rope_scaling["factor"],
+                    original_max_position_embeddings=self.config.rope_scaling["original_max_position_embeddings"],
+                )
+        else:
+            raise NotImplementedError()
