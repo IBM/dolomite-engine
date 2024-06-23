adaabf74e7ffe8913f9fc99d21bb25165da3df74
Mayank Mishra <mayank31398@gmail.com>
2024-06-23 17:57:12 -0400

    math sdpa

--- dolomite_engine/hf_models/models/gpt_ensemble/attention/base.py
+++ dolomite_engine/hf_models/models/gpt_ensemble/attention/base.py
@@ -17,10 +17,12 @@ class EnsembleAttention(Attention):
     def __init__(self, config: GPTEnsembleConfig, causal: bool, layer_idx: int = None) -> None:
         nn.Module.__init__(self)
 
+        self.tp_world_size = config.pretraining_tensor_parallel_size
+
         self.causal = causal
-        self.hidden_size = config.n_embd
-        self.num_heads = config.n_head
-        self.num_key_value_heads = config.num_key_value_heads
+        self.global_hidden_size = config.n_embd
+        self.global_num_heads = config.n_head
+        self.global_num_key_value_heads = config.num_key_value_heads
         self.add_bias = config.add_bias
 
         initializer_range = config.initializer_range
@@ -28,12 +30,21 @@ class EnsembleAttention(Attention):
         n_layer = config.n_layer
         init_method = InitMethod(config.init_method)
 
-        self.head_dim = divide_if_divisible(
-            self.hidden_size,
-            self.num_heads,
-            f"`hidden_size` ({self.hidden_size}) must be divisible by `num_heads` ({self.num_heads})",
+        divide_if_divisible(
+            self.global_hidden_size,
+            self.global_num_heads,
+            f"`embed_dim` ({self.global_hidden_size}) must be divisible by `num_heads` ({self.global_num_heads})",
         )
 
+        self.hidden_size = divide_if_divisible(
+            self.global_hidden_size, self.tp_world_size, "hidden_size should be divisible by TP world size"
+        )
+
+        self.num_heads = divide_if_divisible(
+            self.global_num_heads, self.tp_world_size, "num_heads must be divisible by TP world size"
+        )
+
+        self.head_dim = divide_if_divisible(self.hidden_size, self.num_heads, "")
         self.attention_head_type = AttentionHeadType(config.attention_head_type)
 
         self.position_embedding_type = PositionEmbeddingType(config.position_embedding_type)
@@ -47,27 +58,37 @@ class EnsembleAttention(Attention):
         )
 
         if self.attention_head_type == AttentionHeadType.mha:
-            if self.num_key_value_heads is None:
-                self.num_key_value_heads = self.num_heads
+            if self.global_num_key_value_heads is None:
+                self.global_num_key_value_heads = self.global_num_heads
 
             assert (
-                self.num_heads == self.num_key_value_heads
+                self.global_num_heads == self.global_num_key_value_heads
             ), f"{self.__class__.__name__} should have same number of heads for query, keys and values"
+
+            self.num_key_value_heads = self.num_heads
         elif self.attention_head_type == AttentionHeadType.gqa:
             assert (
-                self.num_key_value_heads is not None
+                self.global_num_key_value_heads is not None
             ), "`num_key_value_heads` needs to be specified with GroupedQueryAttention"
 
+            assert self.global_num_key_value_heads > 1, (
+                "GroupedQueryAttention should have more than 1 head for keys and values, use MultiQueryAttention class if "
+                "you want to use 1 head for keys and values"
+            )
+
             divide_if_divisible(
-                self.num_heads,
-                self.num_key_value_heads,
-                f"`num_heads` ({self.num_heads}) should be a multiple of `num_key_value_heads` ({self.num_key_value_heads})",
+                self.global_num_heads,
+                self.global_num_key_value_heads,
+                f"`num_heads` ({self.global_num_heads}) should be a multiple of `num_key_value_heads` ({self.global_num_key_value_heads})",
+            )
+
+            self.num_key_value_heads = divide_if_divisible(
+                self.global_num_key_value_heads,
+                self.tp_world_size,
+                f"`num_key_value_heads` ({self.global_num_key_value_heads}) must be divisible by `tensor_parallel_world_size` ({self.tp_world_size})",
             )
         elif self.attention_head_type == AttentionHeadType.mqa:
-            if self.num_key_value_heads is None:
-                self.num_key_value_heads = 1
-
-            assert self.num_key_value_heads == 1, f"{self.__class__.__name__} should have 1 head for keys and values"
+            raise ValueError("mqa is not supported with EnsembleAttention")
         else:
             raise ValueError(f"unexpected attention_head_type ({self.attention_head_type})")
 
@@ -77,13 +98,9 @@ class EnsembleAttention(Attention):
         if init_method == InitMethod.mup:
             std /= math.sqrt(m_width)
         self.c_attn = EnsembleLinear(
-            self.hidden_size,
-            divide_if_divisible(
-                self.hidden_size + 2 * self.num_key_value_heads * self.head_dim,
-                config.pretraining_tensor_parallel_size,
-                "",
-            ),
-            tensor_parallel_size=config.pretraining_tensor_parallel_size,
+            self.global_hidden_size,
+            self.hidden_size + 2 * self.num_key_value_heads * self.head_dim,
+            tensor_parallel_size=self.tp_world_size,
             bias=self.add_bias,
             std=std,
         )
@@ -92,9 +109,9 @@ class EnsembleAttention(Attention):
         if init_method == InitMethod.mup:
             std /= math.sqrt(m_width)
         self.c_proj = EnsembleLinear(
-            divide_if_divisible(self.hidden_size, config.pretraining_tensor_parallel_size, ""),
             self.hidden_size,
-            tensor_parallel_size=config.pretraining_tensor_parallel_size,
+            self.global_hidden_size,
+            tensor_parallel_size=self.tp_world_size,
             bias=self.add_bias,
             std=std,
         )
@@ -105,6 +122,53 @@ class EnsembleAttention(Attention):
         self.attn_dropout = nn.Identity() if self.attn_pdrop == 0 else nn.Dropout(self.attn_pdrop)
         self.resid_dropout = nn.Identity() if self.resid_pdrop == 0 else nn.Dropout(self.resid_pdrop)
 
+    def _prepare_qkv_for_forward_mha(
+        self, hidden_states: torch.Tensor
+    ) -> Tuple[torch.Tensor, torch.Tensor, torch.Tensor]:
+        batch_size, tp_world_size, query_length = hidden_states.shape[:3]
+
+        hidden_states = hidden_states.view(batch_size * tp_world_size, query_length, self.num_heads, -1)
+        hidden_states = hidden_states.transpose(1, 2)
+
+        query, key, value = hidden_states.chunk(3, dim=-1)
+
+        return query, key, value
+
+    def _prepare_qkv_for_forward_gqa(
+        self, hidden_states: torch.Tensor
+    ) -> Tuple[torch.Tensor, torch.Tensor, torch.Tensor]:
+        batch_size, query_length = hidden_states.shape[:-1]
+
+        hidden_states = hidden_states.view(batch_size, query_length, self.num_key_value_heads, -1)
+
+        query, key, value = hidden_states.split(
+            ((self.num_heads // self.num_key_value_heads) * self.head_dim, self.head_dim, self.head_dim), dim=-1
+        )
+
+        # this needs to be a reshape instead of view sadly
+        query = query.reshape(batch_size, query_length, -1, self.head_dim)
+
+        query = query.transpose(1, 2)
+        key = key.transpose(1, 2)
+        value = value.transpose(1, 2)
+
+        return query, key, value
+
+    def _prepare_qkv_for_forward_mqa(
+        self, hidden_states: torch.Tensor
+    ) -> Tuple[torch.Tensor, torch.Tensor, torch.Tensor]:
+        batch_size, query_length = hidden_states.shape[:-1]
+
+        query, key, value = hidden_states.split((self.hidden_size, self.head_dim, self.head_dim), dim=-1)
+
+        query = query.view(batch_size, query_length, self.num_heads, -1)
+
+        query = query.transpose(1, 2)
+        key = key.unsqueeze(1)
+        value = value.unsqueeze(1)
+
+        return query, key, value
+
     def forward(
         self,
         hidden_states: torch.Tensor,
@@ -114,10 +178,106 @@ class EnsembleAttention(Attention):
         cu_seqlens: torch.Tensor = None,
         max_seqlen: torch.Tensor = None,
     ) -> torch.Tensor:
-        batch_size, pretraining_tp, query_length, hidden_size = hidden_states.shape
-        hidden_states = hidden_states.view(batch_size * pretraining_tp, query_length, hidden_size)
-        attention_output = super().forward(
-            hidden_states, past_key_values, attention_mask, rope_cos_sin, cu_seqlens, max_seqlen
+        # ==========================================================================================
+        # hidden_states -> (batch_size, 1, query_length, num_heads * head_dim)
+        # ==========================================================================================
+
+        query, key, value = self._prepare_qkv_for_forward(hidden_states)
+
+        # ==========================================================================================
+        # query -> (batch_size * TP, num_heads, query_length, head_dim)
+        # key -> (batch_size * TP, num_key_value_heads, query_length, head_dim)
+        # value -> (batch_size * TP, num_key_value_heads, query_length, head_dim)
+        # ==========================================================================================
+
+        if self.position_embedding_type == PositionEmbeddingType.rope:
+            query = apply_rotary_pos_emb(query, rope_cos_sin)
+            key = apply_rotary_pos_emb(key, rope_cos_sin)
+
+        if past_key_values is not None:
+            key, value = past_key_values.update(key, value, self.layer_idx)
+
+        # ==========================================================================================
+        # query -> (batch_size * TP, num_heads, query_length, head_dim)
+        # key -> (batch_size * TP, num_key_value_heads, key_length, head_dim)
+        # value -> (batch_size * TP, num_key_value_heads, key_length, head_dim)
+        # ==========================================================================================
+
+        key = key.transpose(-1, -2)
+
+        dtype = query.dtype
+        softmax_dtype = torch.float32 if self.attention_softmax_in_fp32 else dtype
+
+        # ==========================================================================================
+        # query -> (batch_size * TP, num_heads, query_length, head_dim)
+        # key -> (batch_size * TP, num_key_value_heads, head_dim, key_length)
+        # value -> (batch_size * TP, num_key_value_heads, key_length, head_dim)
+        # ==========================================================================================
+
+        batch_size_tp = query.shape[0]
+        query_length = query.shape[2]
+        key_length = key.shape[-1]
+
+        key = repeat_key_value(key, self.num_heads, self.num_key_value_heads)
+        value = repeat_key_value(value, self.num_heads, self.num_key_value_heads)
+
+        # Always copies
+        query = query.reshape(batch_size_tp * self.num_heads, query_length, self.head_dim)
+        # No copy when layer_past is provided.
+        key = key.reshape(batch_size_tp * self.num_heads, self.head_dim, key_length)
+
+        # ==========================================================================================
+        # query -> (batch_size * TP * num_heads, query_length, head_dim)
+        # key -> (batch_size * TP * num_heads, head_dim, key_length)
+        # value -> (batch_size * TP, num_heads, key_length, head_dim)
+        # ==========================================================================================
+
+        if attention_mask is None:
+            attn_weights = torch.empty(
+                (batch_size_tp * self.num_heads, query_length, key_length), device=query.device, dtype=query.dtype
+            )
+            beta = 0
+        else:
+            attention_mask = attention_mask.unsqueeze(1)
+            attn_weights = attention_mask.expand(-1, self.tp_world_size, self.num_heads, -1, -1).reshape(
+                -1, query_length, key_length
+            )
+            beta = 1
+
+        attn_weights = torch.baddbmm(attn_weights, query, key, beta=beta, alpha=self._get_softmax_scale(False)).view(
+            batch_size_tp, self.num_heads, query_length, key_length
         )
-        attention_output = attention_output.view(batch_size, pretraining_tp, query_length, hidden_size)
-        return attention_output
+
+        # ==========================================================================================
+        # attn_weights -> (batch_size * TP, num_heads, query_length, key_length)
+        # ==========================================================================================
+
+        attn_weights = F.softmax(attn_weights.to(softmax_dtype), dim=-1).to(dtype)
+        attn_weights = self.attn_dropout(attn_weights)
+
+        # ==========================================================================================
+        # value -> (batch_size * TP, num_heads, key_length, head_dim)
+        # attn_weights -> (batch_size * TP, num_heads, query_length, key_length)
+        # ==========================================================================================
+
+        attn_output = torch.matmul(attn_weights, value)
+
+        # ==========================================================================================
+        # attn_output -> (batch_size * TP, num_heads, query_length, head_dim)
+        # ==========================================================================================
+
+        attn_output = attn_output.transpose(1, 2)
+        attn_output = attn_output.reshape(-1, self.tp_world_size, query_length, self.num_heads * self.head_dim)
+
+        # ==========================================================================================
+        # attn_output -> (batch_size, TP, query_length, num_heads * head_dim)
+        # ==========================================================================================
+
+        attn_output = self.c_proj(attn_output)
+        attn_output = self.resid_dropout(attn_output)
+
+        # ==========================================================================================
+        # attn_output -> (batch_size, TP, query_length, num_heads * head_dim)
+        # ==========================================================================================
+
+        return attn_output
--- dolomite_engine/hf_models/models/gpt_ensemble/attention/sdpa.py
+++ dolomite_engine/hf_models/models/gpt_ensemble/attention/sdpa.py
@@ -1,7 +1,9 @@
 import torch
+import torch.nn.functional as F
 from transformers import DynamicCache
 
-from ....modeling_utils import SDPA
+from ....enums import PositionEmbeddingType
+from ....modeling_utils import apply_rotary_pos_emb, repeat_key_value
 from .base import EnsembleAttention
 
 
@@ -15,10 +17,69 @@ class EnsembleSDPA(EnsembleAttention):
         cu_seqlens: torch.Tensor = None,
         max_seqlen: torch.Tensor = None,
     ) -> torch.Tensor:
-        batch_size, pretraining_tp, query_length, hidden_size = hidden_states.shape
-        hidden_states = hidden_states.view(batch_size * pretraining_tp, query_length, hidden_size)
-        attention_output = SDPA.forward(
-            self, hidden_states, past_key_values, attention_mask, rope_cos_sin, cu_seqlens, max_seqlen
+        # ==========================================================================================
+        # hidden_states -> (batch_size, 1, query_length, num_heads * head_dim)
+        # ==========================================================================================
+
+        query, key, value = self._prepare_qkv_for_forward(hidden_states)
+
+        # ==========================================================================================
+        # query -> (batch_size * TP, num_heads, query_length, head_dim)
+        # key -> (batch_size * TP, num_key_value_heads, query_length, head_dim)
+        # value -> (batch_size * TP, num_key_value_heads, query_length, head_dim)
+        # ==========================================================================================
+
+        if self.position_embedding_type == PositionEmbeddingType.rope:
+            query = apply_rotary_pos_emb(query, rope_cos_sin)
+            key = apply_rotary_pos_emb(key, rope_cos_sin)
+
+        if past_key_values is not None:
+            key, value = past_key_values.update(key, value, self.layer_idx)
+
+        # ==========================================================================================
+        # query -> (batch_size * TP, num_heads, query_length, head_dim)
+        # key -> (batch_size * TP, num_key_value_heads, key_length, head_dim)
+        # value -> (batch_size * TP, num_key_value_heads, key_length, head_dim)
+        # ==========================================================================================
+
+        key = repeat_key_value(key, self.num_heads, self.num_key_value_heads)
+        value = repeat_key_value(value, self.num_heads, self.num_key_value_heads)
+
+        # ==========================================================================================
+        # query -> (batch_size * TP, num_heads, query_length, head_dim)
+        # key -> (batch_size * TP, num_heads, key_length, head_dim)
+        # value -> (batch_size * TP, num_heads, key_length, head_dim)
+        # ==========================================================================================
+
+        softmax_scale = self._get_softmax_scale()
+        dropout_p = self.attn_pdrop if self.training else 0
+
+        attention_mask = attention_mask.expand(-1, self.tp_world_size, -1, -1)
+        attention_mask = attention_mask.reshape(-1, *attention_mask.shape[-2:]).unsqueeze(1)
+
+        attn_output = F.scaled_dot_product_attention(
+            query,
+            key,
+            value,
+            attn_mask=attention_mask,
+            dropout_p=dropout_p,
+            is_causal=self.causal if attention_mask is None else False,
+            scale=softmax_scale,
         )
-        attention_output = attention_output.view(batch_size, pretraining_tp, query_length, hidden_size)
-        return attention_output
+
+        # ==========================================================================================
+        # attn_output -> (batch_size * TP, num_heads, query_length, head_dim)
+        # ==========================================================================================
+
+        query_length = attn_output.shape[2]
+        attn_output = attn_output.transpose(1, 2)
+        attn_output = attn_output.reshape(-1, self.tp_world_size, query_length, self.num_heads * self.head_dim)
+
+        # ==========================================================================================
+        # attn_output -> (batch_size, TP, query_length, num_heads * head_dim)
+        # ==========================================================================================
+
+        attn_output = self.c_proj(attn_output)
+        attn_output = self.resid_dropout(attn_output)
+
+        return attn_output
