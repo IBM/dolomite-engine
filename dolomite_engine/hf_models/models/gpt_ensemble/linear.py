44be1e21ab0f2871780e3ef37b1a094ee4495cff
Mayank Mishra <mayank31398@gmail.com>
2024-06-25 21:05:12 -0400

    ensemble linear

--- dolomite_engine/hf_models/models/gpt_ensemble/linear.py
+++ dolomite_engine/hf_models/models/gpt_ensemble/linear.py
@@ -1,8 +1,6 @@
 import torch
 import torch.nn as nn
 
-from ...modeling_utils import ParameterizedLinear
-
 
 class EnsembleLinear(nn.Module):
     def __init__(
@@ -15,21 +13,42 @@ class EnsembleLinear(nn.Module):
         self.tensor_parallel_size = tensor_parallel_size
         self.std = std
 
-        self.linears = nn.ModuleList(
-            [ParameterizedLinear(in_features, out_features, bias=bias, std=std) for _ in range(tensor_parallel_size)]
-        )
+        self.weight = nn.Parameter(torch.empty(self.tensor_parallel_size, self.in_features, self.out_features))
 
+        self.bias = None
+        if bias:
+            self.bias = nn.Parameter(torch.empty(self.tensor_parallel_size, self.out_features))
+
+        self.reset_parameters()
+
+    @torch.compile
     def forward(self, input: torch.Tensor) -> torch.Tensor:
-        output = []
-        for rank in range(self.tensor_parallel_size):
-            if input.shape[1] == 1:
-                input_ = input.squeeze(1)
-            else:
-                input_ = input[:, rank, ...]
+        batch_size, _, sequence_length, _ = input.shape
 
-            output_ = self.linears[rank](input_)
-            output.append(output_)
+        if input.shape[1] != self.tensor_parallel_size:
+            assert input.shape[1] == 1
+            input = input.expand(-1, self.tensor_parallel_size, -1, -1)
 
-        output = torch.stack(output, dim=1)
+        # input -> (batch_size, TP, sequence_length, in_features)
+        input = input.transpose(0, 1)
+        # input -> (TP, batch_size, sequence_length, in_features)
+        input = input.reshape(self.tensor_parallel_size, batch_size * sequence_length, -1)
+        # input -> (TP, batch_size * sequence_length, in_features)
 
-        return output
+        if self.bias is None:
+            input = torch.bmm(input, self.weight)
+        else:
+            input = torch.baddbmm(self.bias.unsqueeze(1), input, self.weight, alpha=1, beta=1)
+
+        # input -> (TP, batch_size * sequence_length, out_features)
+        input = input.reshape(self.tensor_parallel_size, batch_size, sequence_length, -1)
+        # (TP, batch_size, sequence_length, out_features)
+        input = input.transpose(0, 1).contiguous()
+
+        return input
+
+    @torch.no_grad()
+    def reset_parameters(self) -> None:
+        nn.init.normal_(self.weight, mean=0, std=self.std)
+        if self.bias is not None:
+            self.bias.zero_()
---
+++ tests/hf_models/single_gpu/hf_models/gpt_ensemble/linear_test.py
@@ -0,0 +1,69 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from parameterized import parameterized
+from transformers import set_seed
+
+from dolomite_engine.hf_models.models.gpt_ensemble.linear import EnsembleLinear
+
+from ....test_common import TestCommons
+
+
+SEED = 1234
+
+
+class EnsembleLinearTest(TestCommons):
+    @parameterized.expand([False, True])
+    def test_ensemble_linear_correctness(self, bias: bool) -> None:
+        set_seed(SEED)
+
+        tp_world_size = 8
+        in_features = 7
+        out_features = 11
+
+        ensemble_linear = EnsembleLinear(in_features, out_features, tp_world_size, std=0.02, bias=bias)
+        input = torch.randn(4, tp_world_size, 37, in_features)
+
+        if bias:
+            with torch.no_grad():
+                nn.init.normal_(ensemble_linear.bias, mean=0, std=ensemble_linear.std)
+
+        output = []
+        for rank in range(tp_world_size):
+            i = input[:, rank, :, :]
+            w = ensemble_linear.weight[rank, :, :].T
+            b = None if ensemble_linear.bias is None else ensemble_linear.bias[rank, :]
+
+            output.append(F.linear(i, w, b))
+
+        output = torch.stack(output, dim=1)
+        ensemble_output = ensemble_linear(input)
+
+        assert output.equal(ensemble_output)
+
+    @parameterized.expand([False, True])
+    def test_ensemble_linear_correctness_with_broadcast_input(self, bias: bool) -> None:
+        set_seed(SEED)
+
+        tp_world_size = 8
+        in_features = 7
+        out_features = 11
+
+        ensemble_linear = EnsembleLinear(in_features, out_features, tp_world_size, std=0.02, bias=bias)
+        input = torch.randn(4, 1, 37, in_features)
+
+        if bias:
+            with torch.no_grad():
+                nn.init.normal_(ensemble_linear.bias, mean=0, std=ensemble_linear.std)
+
+        output = []
+        for rank in range(tp_world_size):
+            w = ensemble_linear.weight[rank, :, :].T
+            b = None if ensemble_linear.bias is None else ensemble_linear.bias[rank, :]
+
+            output.append(F.linear(input.squeeze(1), w, b))
+
+        output = torch.stack(output, dim=1)
+        ensemble_output = ensemble_linear(input)
+
+        assert output.equal(ensemble_output)
