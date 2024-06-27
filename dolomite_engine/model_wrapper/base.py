0a61ffd0620990fdeda546646e5c0ba61779a14c
Mayank Mishra <mayank31398@gmail.com>
2024-06-27 06:52:30 -0400

    rng

--- dolomite_engine/model_wrapper/base.py
+++ dolomite_engine/model_wrapper/base.py
@@ -9,7 +9,7 @@ from ..arguments import ExportArgs, InferenceArgs, TrainingArgs
 from ..enums import AttentionImplementation, DistributedBackend, GradientCheckpointingMethod, LossMask, Mode
 from ..hf_models import get_tensor_parallel_class, is_custom_model, is_tensor_parallel_compatible_model
 from ..hf_models.modeling_utils import is_glu
-from ..utils import ProcessGroupManager, log_rank_0, string_to_torch_dtype
+from ..utils import CUDA_RNGStatesTracker, ProcessGroupManager, log_rank_0, set_cuda_rng_tracker, string_to_torch_dtype
 
 
 class ModelWrapper(torch.nn.Module):
@@ -58,6 +58,10 @@ class ModelWrapper(torch.nn.Module):
             assert is_tensor_parallel_compatible_model(
                 self.model_class, self.config.model_type
             ), "tensor parallel is not supported with this model"
+
+            rng_tracker = CUDA_RNGStatesTracker()
+            rng_tracker.add(seed=args.random_args.seed)
+            set_cuda_rng_tracker(rng_tracker)
 
         if self.use_padding_free_transformer:
             assert is_custom_model(
@@ -204,10 +208,7 @@ class ModelWrapper(torch.nn.Module):
         if self.model_name is None:
             model_kwargs = {"config": self.config}
         else:
-            model_kwargs = {
-                "pretrained_model_name_or_path": self.model_name,
-                "trust_remote_code": args.model_args.trust_remote_code,
-            }
+            model_kwargs = {"pretrained_model_name_or_path": self.model_name}
 
         if self.attention_implementation is not None:
             model_kwargs["attn_implementation"] = self.attention_implementation.value
@@ -215,6 +216,8 @@ class ModelWrapper(torch.nn.Module):
             model_kwargs["use_padding_free_transformer"] = True
         if self.tensor_parallel_embeddings:
             model_kwargs["tensor_parallel_embeddings"] = True
+        if args.model_args.trust_remote_code:
+            model_kwargs["trust_remote_code"] = True
 
         def _get_model(**extras):
             if self.model_name is None:
