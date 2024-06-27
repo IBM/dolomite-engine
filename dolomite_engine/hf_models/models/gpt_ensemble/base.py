3846f6ac5b770cb646ba1f641b3b0bf7817ba4eb
Mayank Mishra <mayank31398@gmail.com>
2024-06-27 01:27:49 -0400

    rope

--- dolomite_engine/hf_models/models/gpt_ensemble/base.py
+++ dolomite_engine/hf_models/models/gpt_ensemble/base.py
@@ -69,8 +69,10 @@ class GPTEnsembleModel(GPTEnsemblePreTrainedModel, GPTDolomiteModel):
     def _get_rope_cos_sin(
         self, key_length: int, position_ids: torch.Tensor, dtype: torch.dtype, device: torch.device
     ) -> torch.Tensor:
+        rope_cos_sin = super()._get_rope_cos_sin(key_length, position_ids, dtype=dtype, device=device)
+
         if self.position_embedding_type == PositionEmbeddingType.rope:
-            cos, sin = super()._get_rope_cos_sin(key_length, position_ids, dtype)
+            cos, sin = rope_cos_sin
 
             if position_ids.shape[0] != 1:
                 cos = cos.repeat(self.tensor_parallel_size, 1, 1, 1)
