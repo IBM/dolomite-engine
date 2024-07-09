a3b455febc7e50fa9e4d35763c0dc790752d082b
Aditya Prasad <aprasad@umass.edu>
2024-07-09 18:49:21 -0400

    install requirements while installing (#65)
    Co-authored-by: Aditya Prasad <aditya.prasad@ibm.com>
    Co-authored-by: Mayank Mishra <mayank31398@gmail.com>

--- setup.py
+++ setup.py
@@ -1,13 +1,19 @@
 from setuptools import find_packages, setup
 
 
+with open("requirements.txt") as requirements:
+    requirements = requirements.readlines()
+    requirements = [i.strip() for i in requirements]
+
 VERSION = "0.0.1.dev"
 
 setup(
     name="dolomite-engine",
     version=VERSION,
+    install_requires=requirements,
     author="Mayank Mishra",
     author_email="mayank.mishra2@ibm.com",
+    license="Apache License 2.0",
     url="https://github.com/ibm-granite/dolomite-engine",
     packages=find_packages("./"),
     include_package_data=True,
