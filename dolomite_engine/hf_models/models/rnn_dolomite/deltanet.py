2cabfa93959ff9c6014d9d015378958e783a9835
Yikang Shen <yikang.shn@gmail.com>
2024-07-11 15:35:09 +0000

    add parameterized short conv

--- dolomite_engine/hf_models/models/rnn_dolomite/deltanet.py
+++ dolomite_engine/hf_models/models/rnn_dolomite/deltanet.py
@@ -44,6 +44,28 @@ def elu_norm(x):
     return (x / x.sum(-1, keepdim=True)).to(dtype)
 
 
+class ParameterizedShortConvolution(ShortConvolution):
+    def __init__(
+        self,
+        hidden_size: int,
+        kernel_size: int,
+        bias: bool = False,
+        activation: Optional[str] = 'silu',
+        use_causal_conv: Optional[bool] = True,
+        std: Optional[float] = None,
+    ):
+        self.std = std
+        super().__init__(hidden_size, kernel_size, bias, activation, use_causal_conv)
+
+    def reset_parameters(self) -> None:
+        if self.std is None:
+            super().reset_parameters()
+        else:
+            nn.init.normal_(self.weight, mean=0, std=self.std)
+            if self.bias is not None:
+                self.bias.zero_()
+
+
 # https://github.com/IDSIA/recurrent-fwp/blob/master/algorithmic/layers.py#L86C1-L146C1
 class DeltaNet(nn.Module):
     def __init__(
@@ -60,7 +82,6 @@ class DeltaNet(nn.Module):
         norm_eps: float = 1e-5,
         use_short_conv: bool = True,
         conv_size: int = 4,
-        conv_bias: bool = False,
         share_conv_kernel: bool = False,
     ):
         super().__init__()
@@ -70,6 +91,7 @@ class DeltaNet(nn.Module):
         self.qk_norm = qk_norm
         self.use_short_conv = use_short_conv
         self.share_conv_kernel = share_conv_kernel
+        self.initializer_range = config.initializer_range
 
         assert self.qk_activation in ['silu', 'relu', 'elu', 'identity']
         assert self.qk_norm in ['l2', 'sum']
@@ -92,37 +114,46 @@ class DeltaNet(nn.Module):
         assert self.value_dim % self.num_heads == 0, f"value dim must be divisible by num_heads of {self.num_heads}"
 
         initializer_range = config.initializer_range
-        std = initializer_range
+        std_in = initializer_range
         init_method = InitMethod(config.init_method)
         if init_method == InitMethod.mup:
-            std /= math.sqrt(config.m_width)
-        self.q_proj = ParameterizedLinear(self.hidden_size, self.key_dim, bias=False, std=std)
-        self.k_proj = ParameterizedLinear(self.hidden_size, self.key_dim, bias=False, std=std)
-        self.v_proj = ParameterizedLinear(self.hidden_size, self.value_dim, bias=False, std=std)
+            std_in /= math.sqrt(config.m_width)
+        self.q_proj = ParameterizedLinear(self.hidden_size, self.key_dim, bias=False, std=std_in)
+        self.k_proj = ParameterizedLinear(self.hidden_size, self.key_dim, bias=False, std=std_in)
+        self.v_proj = ParameterizedLinear(self.hidden_size, self.value_dim, bias=False, std=std_in)
 
         if use_short_conv:
+            std_conv = initializer_range
             self.conv_size = conv_size
             if share_conv_kernel:
-                self.h_conv1d = ShortConvolution(self.hidden_size, conv_size, activation=None)
+                self.h_conv1d = ParameterizedShortConvolution(self.hidden_size, conv_size, activation=None, std=std_conv)
             else:
-                self.q_conv1d = ShortConvolution(self.key_dim,
+                self.q_conv1d = ParameterizedShortConvolution(self.key_dim,
                                                  conv_size,
-                                                 activation='silu' if qk_activation == 'silu' else None)
-                self.k_conv1d = ShortConvolution(self.key_dim,
+                                                 activation='silu' if qk_activation == 'silu' else None, 
+                                                 std=std_conv)
+                self.k_conv1d = ParameterizedShortConvolution(self.key_dim,
                                                  conv_size,
-                                                 activation='silu' if qk_activation == 'silu' else None)
-                self.v_conv1d = ShortConvolution(self.value_dim, conv_size, activation='silu')
+                                                 activation='silu' if qk_activation == 'silu' else None,
+                                                 std=std_conv)
+                self.v_conv1d = ParameterizedShortConvolution(self.value_dim, conv_size, activation='silu',
+                                                              std=std_conv)
 
         self.use_beta = use_beta
         self.use_elu = use_elu
         if self.use_beta:
-            self.b_proj = ParameterizedLinear(self.hidden_size, self.num_heads, bias=False, std=std)
+            self.b_proj = ParameterizedLinear(self.hidden_size, self.num_heads, bias=False, std=std_in)
         self.o_norm = RMSNorm(self.head_v_dim, eps=norm_eps)
 
-        std = initializer_range / math.sqrt(2 * config.n_layer)
+        std_out = initializer_range / math.sqrt(2 * config.n_layer)
         if init_method == InitMethod.mup:
-            std /= math.sqrt(config.m_width)
-        self.o_proj = ParameterizedLinear(self.value_dim, self.hidden_size, bias=False, std=std)
+            std_out /= math.sqrt(config.m_width)
+        self.o_proj = ParameterizedLinear(self.value_dim, self.hidden_size, bias=False, std=std_out)
+
+    @torch.no_grad()
+    def reset_parameters(self) -> None:
+        std = self.initializer_range
+        self.q_conv1d.reset_parameters()
 
     def forward(
         self,
