d0f088df4684c67c4c9eccb85670fb949038382e
Yikang Shen <yikang.shn@gmail.com>
2024-07-11 20:05:59 +0000

    add support for nope

--- dolomite_engine/hf_models/models/rnn_dolomite/base.py
+++ dolomite_engine/hf_models/models/rnn_dolomite/base.py
@@ -602,6 +602,8 @@ class RNNDolomiteModel(RNNDolomitePreTrainedModel):
                     scale=self.config.rope_scaling["factor"],
                     original_max_position_embeddings=self.config.rope_scaling["original_max_position_embeddings"],
                 )
+        elif self.position_embedding_type == PositionEmbeddingType.nope:
+            pass
         else:
             raise NotImplementedError()
 
