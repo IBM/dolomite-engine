defc128862d103cfa5394301f63b2ba4a770bd45
Mayank Mishra <mayank31398@gmail.com>
2024-07-11 21:08:32 -0400

    guard class

--- dolomite_engine/hf_models/models/rnn_dolomite/attention/deltanet.py
+++ dolomite_engine/hf_models/models/rnn_dolomite/attention/deltanet.py
@@ -46,26 +46,28 @@ def elu_norm(x):
     return (x / x.sum(-1, keepdim=True)).to(dtype)
 
 
-class ParameterizedShortConvolution(ShortConvolution):
-    def __init__(
-        self,
-        hidden_size: int,
-        kernel_size: int,
-        bias: bool = False,
-        activation: Optional[str] = "silu",
-        use_causal_conv: Optional[bool] = True,
-        std: Optional[float] = None,
-    ):
-        self.std = std
-        super().__init__(hidden_size, kernel_size, bias, activation, use_causal_conv)
+if is_fla_available():
 
-    def reset_parameters(self) -> None:
-        if self.std is None:
-            super().reset_parameters()
-        else:
-            nn.init.normal_(self.weight, mean=0, std=self.std)
-            if self.bias is not None:
-                self.bias.zero_()
+    class ParameterizedShortConvolution(ShortConvolution):
+        def __init__(
+            self,
+            hidden_size: int,
+            kernel_size: int,
+            bias: bool = False,
+            activation: Optional[str] = "silu",
+            use_causal_conv: Optional[bool] = True,
+            std: Optional[float] = None,
+        ):
+            self.std = std
+            super().__init__(hidden_size, kernel_size, bias, activation, use_causal_conv)
+
+        def reset_parameters(self) -> None:
+            if self.std is None:
+                super().reset_parameters()
+            else:
+                nn.init.normal_(self.weight, mean=0, std=self.std)
+                if self.bias is not None:
+                    self.bias.zero_()
 
 
 # https://github.com/IDSIA/recurrent-fwp/blob/master/algorithmic/layers.py#L86C1-L146C1
