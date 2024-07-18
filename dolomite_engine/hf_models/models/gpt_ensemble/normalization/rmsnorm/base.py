cc298c40aca8845a26d9846cee7d1534ccd2975a
Mayank Mishra <mayank31398@gmail.com>
2024-07-18 12:01:36 -0400

    rms fix

--- dolomite_engine/hf_models/models/gpt_ensemble/normalization/rmsnorm/base.py
+++ dolomite_engine/hf_models/models/gpt_ensemble/normalization/rmsnorm/base.py
@@ -10,9 +10,25 @@ class EnsembleRMSNorm(RMSNorm):
     def __init__(self, normalized_shape: int, tp_world_size: int, eps: float = 1e-6) -> None:
         nn.Module.__init__(self)
 
-        self.weight = nn.Parameter(torch.ones(tp_world_size, 1, 1, normalized_shape))
+        self.tp_world_size = tp_world_size
+
+        self.weight = nn.Parameter(torch.ones(tp_world_size * normalized_shape))
         self.eps = eps
 
         if isinstance(normalized_shape, numbers.Integral):
             normalized_shape = (normalized_shape,)
         self.normalized_shape = normalized_shape
+
+    def forward(self, input: torch.Tensor) -> torch.Tensor:
+        input_dtype = input.dtype
+
+        input = input.to(torch.float32)
+        variance = input.pow(2).mean(-1, keepdim=True)
+        input = input * torch.rsqrt(variance + self.eps)
+
+        input = input.to(input_dtype)
+
+        weight = self.weight.view(self.tp_world_size, self.normalized_shape[0]).unsqueeze(1).unsqueeze(1)
+        input = weight * input
+
+        return input
