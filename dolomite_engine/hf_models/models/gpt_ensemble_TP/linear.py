58a1e96ab8b81abfa681993abf32f481a2a41c43
Mayank Mishra <mayank31398@gmail.com>
2024-07-18 13:37:00 -0400

    rms fix

--- dolomite_engine/hf_models/models/gpt_ensemble_TP/linear.py
+++ dolomite_engine/hf_models/models/gpt_ensemble_TP/linear.py
@@ -11,14 +11,14 @@ class EnsembleRowParallelLinear(RowParallelLinear):
         input = ensemble_reduce_from_tensor_parallel_region(input)
         return input
 
-    def load_from_safetensors_weights_manager(
-        self, safetensors_weight_manager: SafeTensorsWeightsManager, prefix: str = ""
-    ) -> None:
-        weight = safetensors_weight_manager.get_slice(prefix + "weight")
-        weight = tensor_parallel_split_safetensor_slice(weight, dim=1)
-        state_dict = {"weight": weight}
+    # def load_from_safetensors_weights_manager(
+    #     self, safetensors_weight_manager: SafeTensorsWeightsManager, prefix: str = ""
+    # ) -> None:
+    #     weight = safetensors_weight_manager.get_slice(prefix + "weight")
+    #     weight = tensor_parallel_split_safetensor_slice(weight, dim=1)
+    #     state_dict = {"weight": weight}
 
-        if self.bias is not None:
-            state_dict["bias"] = safetensors_weight_manager.get_tensor(prefix + "bias")
+    #     if self.bias is not None:
+    #         state_dict["bias"] = safetensors_weight_manager.get_tensor(prefix + "bias")
 
-        self.load_state_dict(state_dict)
+    #     self.load_state_dict(state_dict)
