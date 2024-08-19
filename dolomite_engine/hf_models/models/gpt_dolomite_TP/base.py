b5507c23f491d9abe328fd25c472e9c065075f48
Mayank Mishra <mayank31398@gmail.com>
2024-08-18 20:09:27 -0400

    inherit TP

--- dolomite_engine/hf_models/models/gpt_dolomite_TP/base.py
+++ dolomite_engine/hf_models/models/gpt_dolomite_TP/base.py
@@ -13,12 +13,6 @@ class GPTDolomitePreTrainedModel_TP(PreTrainedModelMixin_TP):
     layer_class = GPTDolomiteBlock_TP
     _no_split_modules = ["GPTDolomiteBlock_TP"]
 
-    def __init__(self, config: GPTDolomiteConfig, *args, **kwargs):
-        super().__init__(config, *args, **kwargs)
-
-        self.tensor_parallel_word_embeddings = kwargs.get("tensor_parallel_word_embeddings", False)
-        self.sequence_parallel = kwargs.get("sequence_parallel", False)
-
 
 class GPTDolomiteModel_TP(GPTDolomitePreTrainedModel_TP, BaseModelMixin_TP):
     def __init__(self, config: GPTDolomiteConfig, **kwargs) -> None:
--- dolomite_engine/hf_models/models/gpt_ensemble_TP/main.py
+++ dolomite_engine/hf_models/models/gpt_ensemble_TP/main.py
@@ -1,31 +1,6 @@
-from __future__ import annotations
-
-from ...modeling_utils import ParameterizedLinear
-from ...modeling_utils_TP import LMHead_TP
-from ..gpt_dolomite import GPTDolomiteConfig
-from ..gpt_dolomite_TP import GPTDolomiteForCausalLM_TP
+from ...mixins import CausalLMModelMixin_TP
 from .base import GPTEnsembleModel_TP, GPTEnsemblePreTrainedModel_TP
 
 
-class GPTEnsembleForCausalLM_TP(GPTEnsemblePreTrainedModel_TP, GPTDolomiteForCausalLM_TP):
-    def __init__(self, config: GPTDolomiteConfig, tensor_parallel_embeddings: bool = False, **kwargs) -> None:
-        GPTEnsemblePreTrainedModel_TP.__init__(self, config, **kwargs)
-
-        self.tensor_parallel_embeddings = tensor_parallel_embeddings
-        self.vocab_size = config.vocab_size
-
-        self.transformer = GPTEnsembleModel_TP(config, tensor_parallel_embeddings=tensor_parallel_embeddings, **kwargs)
-
-        if not self._tied_word_embeddings:
-            if self.tensor_parallel_embeddings:
-                self.lm_head = LMHead_TP(config.vocab_size, config.n_embd, std=config.initializer_range)
-            else:
-                self.lm_head = ParameterizedLinear(
-                    config.n_embd, config.vocab_size, bias=False, std=config.initializer_range
-                )
-
-        self.m_width = config.m_width
-        self.upcast_logits_for_loss = config.upcast_logits_for_loss
-
-        # Initialize weights and apply final processing
-        self.post_init()
+class GPTEnsembleForCausalLM_TP(GPTEnsemblePreTrainedModel_TP, CausalLMModelMixin_TP):
+    base_model_class = GPTEnsembleModel_TP
