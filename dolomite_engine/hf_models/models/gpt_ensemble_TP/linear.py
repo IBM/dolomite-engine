33359c6ba7132b17a4cedaf146f7bc14593c55a9
Mayank Mishra <mayank31398@gmail.com>
2024-09-02 14:21:00 -0400

    e

--- dolomite_engine/hf_models/models/gpt_ensemble_TP/linear.py
+++ dolomite_engine/hf_models/models/gpt_ensemble_TP/linear.py
@@ -1,5 +1,6 @@
 import torch
 import torch.distributed
+import torch.nn.functional as F
 from torch.distributed._tensor.placement_types import Partial, Shard
 
 from ...modeling_utils_TP import RowParallelLinear, dtensor_to_tensor, tensor_to_dtensor
@@ -9,7 +10,10 @@ class EnsembleRowParallelLinear(RowParallelLinear):
     def forward(self, input: torch.Tensor, residual: torch.Tensor) -> torch.Tensor:
         input = tensor_to_dtensor(input, current_placement=Shard(-1))
         residual = tensor_to_dtensor(residual / self.tp_world_size, current_placement=Partial())
-        input = super().forward(input)
+
+        input = F.linear(input, self.weight, self.bias)
         input = input + residual
+
         input = dtensor_to_tensor(input, desired_placement=self.output_placement)
+
         return input
