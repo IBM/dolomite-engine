5905ec0e32aa772a74efcad971e66ffa6bb22f92
Mayank Mishra <mayank31398@gmail.com>
2024-09-11 18:06:02 -0400

    drop densemoe
    Signed-off-by: Mayank Mishra <mayank31398@gmail.com>

--- dolomite_engine/hf_models/models/moe_dolomite/moe/base.py
+++ dolomite_engine/hf_models/models/moe_dolomite/moe/base.py
@@ -7,7 +7,6 @@ import torch.nn.functional as F
 from ....enums import InitMethod
 from ....modeling_utils import ParameterizedLinear, get_activation_function, is_glu
 from ..config import MoEDolomiteConfig
-from .utils import compute_switchloss
 
 
 class ParameterizedExperts(nn.Module):
@@ -127,7 +126,7 @@ class SparseMoE(nn.Module):
 
         hidden_states = self.dropout(hidden_states)
 
-        aux_loss = compute_switchloss(
+        aux_loss = self._compute_switch_loss(
             logits=router_logits, probs=torch.softmax(router_logits, dim=-1), topk_idxs=selected_experts
         )
 
@@ -191,3 +190,18 @@ class SparseMoE(nn.Module):
             x, indices = x.topk(self.top_k, dim=-1)
 
         return x, indices
+
+    def _compute_switch_loss(self, logits: torch.Tensor, probs: torch.Tensor, topk_idxs: torch.Tensor) -> torch.Tensor:
+        logits = logits.view(-1, logits.size(-1))
+        probs = probs.view(-1, probs.size(-1))
+
+        num_experts = logits.size(1)
+        acc_probs = probs.sum(0)
+        freq = torch.bincount(topk_idxs.flatten(), minlength=num_experts).to(dtype=logits.dtype)
+
+        switch_loss = num_experts * (F.normalize(acc_probs, p=1, dim=0) * F.normalize(freq, p=1, dim=0)).sum()
+        z_loss = (torch.logsumexp(logits, dim=-1) ** 2).mean()
+
+        loss = switch_loss + 0.1 * z_loss
+
+        return loss
