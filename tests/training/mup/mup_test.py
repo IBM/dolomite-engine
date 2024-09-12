db3cc069162d346611d1bf3456ba4a6ce198c0a2
Mayank Mishra <mayank31398@gmail.com>
2024-09-12 16:06:44 -0400

    refactor
    Signed-off-by: Mayank Mishra <mayank31398@gmail.com>

--- tests/training/mup/mup_test.py
+++ tests/training/mup/mup_test.py
@@ -13,22 +13,13 @@ from ..test_commons import TestCommons
 
 class MuPTest(TestCommons):
     def test_gpt_dolomite_mup(self) -> None:
-        args = TestCommons.load_training_args_for_unit_tests("mup/gpt_dolomite_config.yml")
-
-        with (
-            torch.device("meta"),
-            ProcessGroupManager.set_dummy_tensor_parallel_world_size(1),
-            ProcessGroupManager.set_dummy_tensor_parallel_rank(0),
-        ):
-            model = get_model(args, Mode.training)
-
-        _, names = _get_param_groups(model, args.optimizer_args.class_args, params_group_method=ParamsGroupMethod.mup)
-
-        expected_group = json.load(open(os.path.join(os.path.dirname(__file__), "gpt_dolomite_groups.json"), "r"))
-        assert expected_group == names
+        self._test_mup("gpt_dolomite_config.yml", "gpt_dolomite_groups.json")
 
     def test_moe_dolomite_mup(self) -> None:
-        args = TestCommons.load_training_args_for_unit_tests("mup/moe_dolomite_config.yml")
+        self._test_mup("moe_dolomite_config.yml", "moe_dolomite_groups.json")
+
+    def _test_mup(self, config_filename: str, expected_groups_filename: str) -> None:
+        args = TestCommons.load_training_args_for_unit_tests(os.path.join("mup", config_filename))
 
         with (
             torch.device("meta"),
@@ -39,5 +30,5 @@ class MuPTest(TestCommons):
 
         _, names = _get_param_groups(model, args.optimizer_args.class_args, params_group_method=ParamsGroupMethod.mup)
 
-        expected_group = json.load(open(os.path.join(os.path.dirname(__file__), "moe_dolomite_groups.json"), "r"))
+        expected_group = json.load(open(os.path.join(os.path.dirname(__file__), expected_groups_filename), "r"))
         assert expected_group == names
