b25fac47ffc786d81a4d47c778a04f5760f08dd2
Shawn Tan <shawn@wtf.sg>
2024-09-13 04:41:22 +0000

    ScatterMoE with Sequence Parallel, tested for padding free
    Signed-off-by: Shawn Tan <shawntan@ibm.com>

--- dolomite_engine/hf_models/models/moe_dolomite_TP/moe_TP/scatter.py
+++ dolomite_engine/hf_models/models/moe_dolomite_TP/moe_TP/scatter.py
@@ -3,12 +3,13 @@ import math
 import torch
 import torch.distributed
 import torch.nn as nn
+import torch.nn.functional as F
 from torch.distributed._tensor.api import DTensor
 from torch.distributed._tensor.placement_types import Partial, Replicate, Shard
 
 from .....utils import ProcessGroupManager, is_kernel_hyperdrive_available
 from ....enums import InitMethod
-from ....modeling_utils import get_activation_function, is_glu
+from ....modeling_utils import ParameterizedLinear, get_activation_function, is_glu
 from ....modeling_utils_TP import (
     DTensorModule,
     ReplicatedLinear,
@@ -26,7 +27,28 @@ if is_kernel_hyperdrive_available():
     from khd.kernels.scattermoe.triton_implementation import scattered_experts
 
 
-class ColumnParallelScatteredExperts(ParameterizedScatteredExperts, DTensorModule):
+class ReplicatedRouter(ParameterizedLinear):
+    def __init__(
+        self,
+        in_features: int,
+        out_features: int,
+        bias: bool = False,
+        device: torch.device | None = None,
+        dtype: torch.dtype | None = None,
+        std: float | None = None,
+        use_padding_free_transformer: bool = False,
+        sequence_parallel: bool = False,
+    ) -> None:
+        super().__init__(in_features, out_features, bias, device, dtype, std)
+
+        self.weight = nn.Parameter(
+            DTensor.from_local(
+                self.weight, device_mesh=ProcessGroupManager.get_tensor_parallel_mesh(), placements=[Replicate()]
+            )
+        )
+
+
+class ColumnParallelScatteredExperts(ParameterizedScatteredExperts):
     def __init__(
         self,
         num_experts: int,
@@ -65,8 +87,8 @@ class ColumnParallelScatteredExperts(ParameterizedScatteredExperts, DTensorModul
                 run_check=False,
             )
         )
-
-        self.input_placement = get_module_placements(use_padding_free_transformer, sequence_parallel)
+        # Put in MLP
+        # self.input_placement = get_module_placements(use_padding_free_transformer, sequence_parallel)
 
     def forward(
         self,
@@ -82,8 +104,6 @@ class ColumnParallelScatteredExperts(ParameterizedScatteredExperts, DTensorModul
     ):
         # F.linear manually triggers an all gather for sequence parallel but custom kernels are not aware of the placements
         # so we manually call an all gather here
-        inputs = tensor_to_dtensor(inputs, current_placement=self.input_placement)
-        inputs = dtensor_to_tensor(inputs, desired_placement=Replicate(), grad_placement=Partial())
 
         weight = self.weight.to_local()
 
@@ -143,7 +163,7 @@ class RowParallelScatteredExperts(ParameterizedScatteredExperts, DTensorModule):
             )
         )
 
-        self.output_placement = get_module_placements(use_padding_free_transformer, sequence_parallel)
+        # self.output_placement = get_module_placements(use_padding_free_transformer, sequence_parallel)
 
     def forward(
         self,
@@ -172,9 +192,6 @@ class RowParallelScatteredExperts(ParameterizedScatteredExperts, DTensorModule):
             grouped_out,
         )
 
-        inputs = tensor_to_dtensor(inputs, current_placement=Partial())
-        inputs = dtensor_to_tensor(inputs, desired_placement=self.output_placement)
-
         return inputs
 
 
@@ -183,6 +200,7 @@ class ScatterMoE_TP(ScatterMoE, DTensorModule):
         self,
         config: MoEDolomiteConfig,
         use_padding_free_transformer: bool,
+        sequence_parallel: bool = False,
         layer_idx: int | None = None,
     ) -> None:
         nn.Module.__init__(self)
@@ -203,13 +221,13 @@ class ScatterMoE_TP(ScatterMoE, DTensorModule):
         init_method = InitMethod(config.init_method)
         residual_dropout = config.resid_pdrop
 
-        self.gate = ReplicatedLinear(
+        self.gate = ReplicatedRouter(
             in_features=self.hidden_size,
             out_features=config.num_experts,
             bias=False,
             std=config.initializer_range,
             use_padding_free_transformer=use_padding_free_transformer,
-            sequence_parallel=False,
+            sequence_parallel=False,  # replicate even if SP
         )
 
         std = initializer_range
@@ -238,3 +256,35 @@ class ScatterMoE_TP(ScatterMoE, DTensorModule):
         )
 
         self.dropout = nn.Identity() if residual_dropout == 0 else nn.Dropout(residual_dropout)
+        self.input_placement = get_module_placements(use_padding_free_transformer, sequence_parallel)
+        self.output_placement = self.input_placement
+
+    def _compute_routing_weights(self, hidden_states: torch.Tensor) -> tuple[torch.Tensor]:
+        # hidden_states -> (total_q, hidden_size)
+        router_logits = self.gate(hidden_states)
+        router_logits = dtensor_to_tensor(router_logits, desired_placement=Replicate(), grad_placement=Partial())
+        # router_logits -> (total_q, num_experts)
+
+        router_weights, selected_experts = self._get_topk(router_logits)
+        router_weights = F.softmax(router_weights.float(), dim=-1)
+
+        # we cast back to the input dtype
+        router_weights = router_weights.type_as(hidden_states)
+
+        return router_logits, router_weights, selected_experts
+
+    def forward(self, hidden_states: torch.Tensor) -> torch.Tensor:
+        hidden_states = tensor_to_dtensor(hidden_states, current_placement=self.input_placement)
+
+        router_logits, router_weights, selected_experts = self._compute_routing_weights(hidden_states)
+
+        hidden_states = dtensor_to_tensor(hidden_states, desired_placement=Replicate(), grad_placement=Partial())
+
+        hidden_states = self._compute_experts(hidden_states, router_weights, selected_experts)
+
+        hidden_states = tensor_to_dtensor(hidden_states, current_placement=Partial())
+        hidden_states = dtensor_to_tensor(
+            hidden_states, desired_placement=self.output_placement, grad_placement=self.output_placement
+        )
+        hidden_states = self.dropout(hidden_states)
+        return hidden_states, router_logits
