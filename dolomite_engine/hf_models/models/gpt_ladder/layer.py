2817484394c476e6345a6f0379f5b7fdf61ae963
Mayank Mishra <mayank31398@gmail.com>
2024-09-16 22:27:51 -0400

    ladder
    Signed-off-by: Mayank Mishra <mayank31398@gmail.com>

--- dolomite_engine/hf_models/models/gpt_ladder/layer.py
+++ dolomite_engine/hf_models/models/gpt_ladder/layer.py
@@ -28,9 +28,15 @@ class GPTLadderBlock(GPTDolomiteBlock):
             max_seqlen=max_seqlen,
         )
 
+        if self.m_residual is not None:
+            current_attention_out = current_attention_out * self.m_residual
+
         residual = residual + previous_mlp_out
 
         current_mlp_out = self.ln_2(residual)
         current_mlp_out = self.mlp(current_mlp_out)
+
+        if self.m_residual is not None:
+            current_mlp_out = current_mlp_out * self.m_residual
 
         return current_attention_out, current_mlp_out, residual
