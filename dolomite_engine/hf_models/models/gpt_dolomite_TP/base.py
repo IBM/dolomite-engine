9ddb80950c8ad490955af80f2d8a9ed648c8f5d5
Mayank Mishra <mayank31398@gmail.com>
2024-09-22 01:50:24 -0400

    copy __init__
    Signed-off-by: Mayank Mishra <mayank31398@gmail.com>

--- dolomite_engine/hf_models/models/gpt_dolomite_TP/base.py
+++ dolomite_engine/hf_models/models/gpt_dolomite_TP/base.py
@@ -21,9 +21,7 @@ class GPTDolomitePreTrainedModel_TP(PreTrainedModelMixin_TP):
 
 
 class GPTDolomiteModel_TP(GPTDolomitePreTrainedModel_TP, BaseModelMixin_TP):
-    def __init__(self, config: GPTDolomiteConfig, **kwargs) -> None:
-        super().__init__(config, **kwargs)
-
+    def _init_model(self, config: GPTDolomiteConfig, **kwargs) -> None:
         self.attention_head_type = AttentionHeadType(config.attention_head_type)
         self.embed_dim = config.hidden_size
         self.num_heads = config.num_attention_heads
