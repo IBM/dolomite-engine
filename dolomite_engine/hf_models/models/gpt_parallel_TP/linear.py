4c475d64962e28c233c686615d9c79aad6f4d941
Mayank Mishra <mayank31398@gmail.com>
2024-09-22 21:42:05 -0400

    parallel_TP
    Signed-off-by: Mayank Mishra <mayank31398@gmail.com>

--- dolomite_engine/hf_models/models/gpt_parallel_TP/linear.py
+++ dolomite_engine/hf_models/models/gpt_parallel_TP/linear.py
@@ -1,11 +1,12 @@
 import torch
+from torch.distributed._tensor.api import DTensor
 from torch.distributed._tensor.placement_types import Shard
 
 from ...modeling_utils_TP import RowParallelLinear, tensor_to_dtensor
 
 
 class ParallelRowParallelLinear(RowParallelLinear):
-    def forward(self, input: torch.Tensor) -> torch.Tensor:
+    def forward(self, input: torch.Tensor) -> DTensor:
         input = tensor_to_dtensor(input, current_placement=Shard(-1))
         input = super().forward(input)
         return input
