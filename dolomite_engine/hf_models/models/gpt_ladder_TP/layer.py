ddc44b8ae6f589f645609160aa2c5b2e7895a5ee
Mayank Mishra <mayank31398@gmail.com>
2024-09-23 02:28:17 -0400

    ladder_TP layer
    Signed-off-by: Mayank Mishra <mayank31398@gmail.com>

--- dolomite_engine/hf_models/models/gpt_ladder_TP/layer.py
+++ dolomite_engine/hf_models/models/gpt_ladder_TP/layer.py
@@ -1,4 +1,26 @@
+from torch import Tensor
+from transformers import DynamicCache
+
 from ..gpt_dolomite_TP.layer import GPTDolomiteBlock_TP
+from ..gpt_ladder.layer import GPTLadderBlock
 
 
-class GPTLadderBlock_TP(GPTDolomiteBlock_TP): ...
+class GPTLadderBlock_TP(GPTDolomiteBlock_TP):
+    def forward(
+        self,
+        hidden_states: Tensor,
+        past_key_values: DynamicCache | None = None,
+        attention_mask: Tensor | None = None,
+        rope_cos_sin: Tensor | None = None,
+        cu_seqlens: Tensor | None = None,
+        max_seqlen: Tensor | None = None,
+    ) -> tuple[Tensor]:
+        return GPTLadderBlock.forward(
+            self,
+            hidden_states=hidden_states,
+            past_key_values=past_key_values,
+            attention_mask=attention_mask,
+            rope_cos_sin=rope_cos_sin,
+            cu_seqlens=cu_seqlens,
+            max_seqlen=max_seqlen,
+        )
