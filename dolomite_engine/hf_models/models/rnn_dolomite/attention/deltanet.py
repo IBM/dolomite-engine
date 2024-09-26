41539e2eda2cb62fe2caba834e788ae644867723
Mayank Mishra <mayank31398@gmail.com>
2024-09-26 15:54:00 -0400

    use short-conv locally
    Signed-off-by: Mayank Mishra <mayank31398@gmail.com>

--- dolomite_engine/hf_models/models/rnn_dolomite/attention/deltanet.py
+++ dolomite_engine/hf_models/models/rnn_dolomite/attention/deltanet.py
@@ -9,6 +9,7 @@ from .....utils import is_einops_available, is_fla_available
 from ....config import CommonConfig
 from ....enums import InitMethod
 from ....modeling_utils import ParameterizedLinear, get_normalization_function
+from .convolution import ParameterizedShortConvolution
 
 
 if is_einops_available():
@@ -16,32 +17,7 @@ if is_einops_available():
 
 if is_fla_available():
     from fla.models.utils import Cache as FLACache
-    from fla.modules import ShortConvolution
     from fla.ops.delta_rule import chunk_delta_rule, fused_chunk_delta_rule, fused_recurrent_delta_rule
-
-
-if is_fla_available():
-
-    class ParameterizedShortConvolution(ShortConvolution):
-        def __init__(
-            self,
-            hidden_size: int,
-            kernel_size: int,
-            bias: bool = False,
-            activation: str = "silu",
-            use_causal_conv: bool = True,
-            std: float | None = None,
-        ) -> None:
-            self.std = std
-            super().__init__(hidden_size, kernel_size, bias, activation, use_causal_conv)
-
-        def reset_parameters(self) -> None:
-            if self.std is None:
-                super().reset_parameters()
-            else:
-                nn.init.normal_(self.weight, mean=0, std=self.std)
-                if self.bias is not None:
-                    self.bias.zero_()
 
 
 # https://github.com/IDSIA/recurrent-fwp/blob/master/algorithmic/layers.py#L86C1-L146C1
