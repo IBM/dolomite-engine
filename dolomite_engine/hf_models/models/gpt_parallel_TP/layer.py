adc3c1dacc22c439a2036c98134f5e8d5c51f36e
Mayank Mishra <mayank31398@gmail.com>
2024-09-26 06:07:32 -0400

    dtensor free
    Signed-off-by: Mayank Mishra <mayank31398@gmail.com>

--- dolomite_engine/hf_models/models/gpt_parallel_TP/layer.py
+++ dolomite_engine/hf_models/models/gpt_parallel_TP/layer.py
@@ -8,12 +8,12 @@ from ...modeling_utils_TP import (
     dtensor_to_tensor,
     get_attention_module_TP,
     get_module_placements,
+    get_normalization_function_TP,
     reduce_from_tensor_parallel_region,
 )
 from ..gpt_dolomite_TP.mlp import MLP_TP
 from ..gpt_parallel import GPTParallelConfig
 from .linear import ParallelRowParallelLinear
-from .normalization import get_normalization_function_TP
 
 
 class GPTParallelBlock_TP(nn.Module):
@@ -32,7 +32,7 @@ class GPTParallelBlock_TP(nn.Module):
         self.layer_idx = layer_idx
         self.m_residual = config.m_residual
 
-        self.ln = get_normalization_function_TP(
+        self.ln_1 = get_normalization_function_TP(
             config.normalization_function,
             hidden_size,
             eps=config.layer_norm_epsilon,
@@ -46,6 +46,14 @@ class GPTParallelBlock_TP(nn.Module):
             attention_implementation=attention_implementation,
             use_padding_free_transformer=use_padding_free_transformer,
             layer_idx=layer_idx,
+            sequence_parallel=sequence_parallel,
+        )
+        self.ln_2 = get_normalization_function_TP(
+            config.normalization_function,
+            hidden_size,
+            eps=config.layer_norm_epsilon,
+            normalization_implementation=normalization_implementation,
+            use_padding_free_transformer=use_padding_free_transformer,
             sequence_parallel=sequence_parallel,
         )
         self.mlp = MLP_TP(
@@ -67,12 +75,7 @@ class GPTParallelBlock_TP(nn.Module):
     ) -> tuple[torch.Tensor]:
         residual = hidden_states
 
-        hidden_states = self.ln(hidden_states)
-        attention_out, mlp_out = hidden_states.chunk(2)
-
-        attention_out = attention_out.squeeze(0)
-        mlp_out = mlp_out.squeeze(0)
-
+        attention_out = self.ln_1(hidden_states)
         attention_out = self.attn(
             attention_out,
             past_key_values=past_key_values,
@@ -82,6 +85,7 @@ class GPTParallelBlock_TP(nn.Module):
             max_seqlen=max_seqlen,
         )
 
+        mlp_out = self.ln_2(hidden_states)
         mlp_out = self.mlp(mlp_out)
 
         hidden_states = attention_out + mlp_out
