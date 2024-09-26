bd9e055c1e321c9aeddb06c1e1cd377f2efad95b
Mayank Mishra <mayank31398@gmail.com>
2024-09-26 14:16:28 -0400

    rename variable
    Signed-off-by: Mayank Mishra <mayank31398@gmail.com>

--- dolomite_engine/optimization/params_group.py
+++ dolomite_engine/optimization/params_group.py
@@ -12,6 +12,7 @@ from ..hf_models import (
 from ..hf_models.modeling_utils import Attention
 from ..hf_models.models.gpt_dolomite.layer import MLP
 from ..hf_models.models.moe_dolomite.moe import SparseMoE
+from ..hf_models.models.rnn_dolomite.attention import DeltaNet
 from ..model_wrapper import ModelWrapper
 from ..utils import log_rank_0
 
@@ -80,7 +81,7 @@ def get_mup_group_with_names(model: ModelWrapper, optimizer_class_args: dict) ->
 
     # collect parameters with mup learning rate
     for module_name, module in model.named_modules():
-        if isinstance(module, (Attention, MLP, SparseMoE)):
+        if isinstance(module, (Attention, MLP, SparseMoE, DeltaNet)):
             for param_name, param in module.named_parameters():
                 # we don't add bias to mup group
                 if not param_name.endswith(".bias"):
