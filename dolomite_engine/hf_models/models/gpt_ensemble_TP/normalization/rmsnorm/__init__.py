950bceb48527f2c09f81b7c218241f4a8ae24867
Mayank Mishra <mayank31398@gmail.com>
2024-09-26 03:37:33 -0400

    dtensor free
    Signed-off-by: Mayank Mishra <mayank31398@gmail.com>

--- dolomite_engine/hf_models/models/gpt_ensemble_TP/normalization/rmsnorm/__init__.py
+++ dolomite_engine/hf_models/models/gpt_ensemble_TP/normalization/rmsnorm/__init__.py
@@ -6,12 +6,8 @@ from .base import EnsembleRMSNorm_TP
 _RMSNORM_MODULES = {"torch": EnsembleRMSNorm_TP}
 
 
-def get_rmsnorm(
-    normalized_shape: int, tp_world_size: int, eps: float, normalization_implementation: str = "torch"
-) -> nn.LayerNorm:
+def get_rmsnorm(normalized_shape: int, eps: float, normalization_implementation: str = "torch") -> nn.LayerNorm:
     if normalization_implementation in _RMSNORM_MODULES:
-        return _RMSNORM_MODULES[normalization_implementation](
-            normalized_shape=normalized_shape, tp_world_size=tp_world_size, eps=eps
-        )
+        return _RMSNORM_MODULES[normalization_implementation](normalized_shape=normalized_shape, eps=eps)
 
     raise ValueError(f"unexpected `normalization_implementation` {normalization_implementation}")
