6621405dcd04ca34c92df1a4cf5aa372847e057d
Mayank Mishra <mayank31398@gmail.com>
2024-09-26 03:35:39 -0400

    dtensor free
    Signed-off-by: Mayank Mishra <mayank31398@gmail.com>

--- dolomite_engine/hf_models/models/gpt_ensemble_TP/normalization/__init__.py
+++ dolomite_engine/hf_models/models/gpt_ensemble_TP/normalization/__init__.py
@@ -9,14 +9,12 @@ _NORMALIZATION_FUNCTIONS = {"rmsnorm": get_rmsnorm}
 def get_ensemble_normalization_function_TP(
     name: str,
     normalized_shape: int,
-    tp_world_size: int,
     eps: float = 1e-5,
     normalization_implementation: str = "torch",
 ) -> nn.LayerNorm:
     if name in _NORMALIZATION_FUNCTIONS:
         return _NORMALIZATION_FUNCTIONS[name](
             normalized_shape,
-            tp_world_size=tp_world_size,
             eps=eps,
             normalization_implementation=normalization_implementation,
         )
