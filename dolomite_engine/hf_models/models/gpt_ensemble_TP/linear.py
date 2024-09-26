221fa1e3229b9ba408293fb61d16964f3731287b
Mayank Mishra <mayank31398@gmail.com>
2024-09-26 02:30:24 -0400

    dtensor free
    Signed-off-by: Mayank Mishra <mayank31398@gmail.com>

--- dolomite_engine/hf_models/models/gpt_ensemble_TP/linear.py
+++ dolomite_engine/hf_models/models/gpt_ensemble_TP/linear.py
@@ -1,19 +1,37 @@
 import torch
 import torch.distributed
 import torch.nn.functional as F
+from torch.distributed import ReduceOp
 from torch.distributed._tensor.placement_types import Partial, Shard
 
-from ...modeling_utils_TP import RowParallelLinear, dtensor_to_tensor, tensor_to_dtensor
+from ....utils import is_dtensors_enabled
+from ...modeling_utils_TP import (
+    RowParallelLinear,
+    dtensor_to_tensor,
+    reduce_from_tensor_parallel_region,
+    tensor_to_dtensor,
+)
 
 
 class EnsembleRowParallelLinear(RowParallelLinear):
     def forward(self, input: torch.Tensor, residual: torch.Tensor) -> torch.Tensor:
-        input = tensor_to_dtensor(input, current_placement=Shard(-1))
-        residual = tensor_to_dtensor(residual / self.tp_world_size, current_placement=Partial())
+        if is_dtensors_enabled():
+            input = tensor_to_dtensor(input, current_placement=Shard(-1))
+            residual = tensor_to_dtensor(residual / self.tp_world_size, current_placement=Partial())
 
-        input = F.linear(input, self.weight, self.bias)
-        input = input + residual
+            input = F.linear(input, self.weight, self.bias)
+            input = input + residual
 
-        input = dtensor_to_tensor(input, desired_placement=self.output_placement)
+            input = dtensor_to_tensor(input, desired_placement=self.output_placement)
+        else:
+            assert not self.use_padding_free_transformer
+            assert not self.sequence_parallel
+
+            input = F.linear(input, self.weight.to_local(), None)
+            input = input + residual / self.tp_world_size
+            input = reduce_from_tensor_parallel_region(input, op=ReduceOp.SUM)
+
+            if self.bias is not None:
+                input = input + self.bias.to_local()
 
         return input
