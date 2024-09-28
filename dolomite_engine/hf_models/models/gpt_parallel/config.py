deb4ac842586457c7543fe0c3c66d7339591879a
Mayank Mishra <mayank31398@gmail.com>
2024-09-28 00:05:45 -0400

    shared_lm
    Signed-off-by: Mayank Mishra <mayank31398@gmail.com>

--- dolomite_engine/hf_models/models/gpt_parallel/config.py
+++ dolomite_engine/hf_models/models/gpt_parallel/config.py
@@ -3,3 +3,8 @@ from ...config import CommonConfig
 
 class GPTParallelConfig(CommonConfig):
     model_type = "gpt_parallel"
+
+    def __init__(self, shared_layernorm: bool = True, **kwargs) -> None:
+        super().__init__(**kwargs)
+
+        self.shared_layernorm = shared_layernorm
--- dolomite_engine/hf_models/models/gpt_parallel/layer.py
+++ dolomite_engine/hf_models/models/gpt_parallel/layer.py
@@ -1,10 +1,50 @@
 import torch
+import torch.nn as nn
 from transformers import DynamicCache
 
+from ...modeling_utils import get_attention_module, get_normalization_function
 from ..gpt_dolomite.layer import GPTDolomiteBlock
+from ..gpt_dolomite.mlp import MLP
+from ..gpt_parallel import GPTParallelConfig
 
 
 class GPTParallelBlock(GPTDolomiteBlock):
+    def __init__(
+        self,
+        config: GPTParallelConfig,
+        normalization_implementation: str,
+        attention_implementation: str,
+        use_padding_free_transformer: bool,
+        layer_idx: int | None = None,
+    ) -> None:
+        self.shared_layernorm = config.shared_layernorm
+
+        if self.shared_layernorm:
+            nn.Module.__init__(self)
+
+            hidden_size = config.hidden_size
+            self.layer_idx = layer_idx
+            self.m_residual = config.m_residual
+
+            self.ln = get_normalization_function(
+                config.normalization_function,
+                hidden_size,
+                eps=config.layer_norm_epsilon,
+                normalization_implementation=normalization_implementation,
+            )
+            self.attn = get_attention_module(
+                config, True, attention_implementation, use_padding_free_transformer, layer_idx
+            )
+            self.mlp = MLP(config)
+        else:
+            super().__init__(
+                config=config,
+                normalization_implementation=normalization_implementation,
+                attention_implementation=attention_implementation,
+                use_padding_free_transformer=use_padding_free_transformer,
+                layer_idx=layer_idx,
+            )
+
     def forward(
         self,
         hidden_states: torch.Tensor,
@@ -16,18 +56,34 @@ class GPTParallelBlock(GPTDolomiteBlock):
     ) -> tuple[torch.Tensor]:
         residual = hidden_states
 
-        attention_out = self.ln_1(hidden_states)
-        attention_out = self.attn(
-            attention_out,
-            past_key_values=past_key_values,
-            attention_mask=attention_mask,
-            rope_cos_sin=rope_cos_sin,
-            cu_seqlens=cu_seqlens,
-            max_seqlen=max_seqlen,
-        )
+        if self.shared_layernorm:
+            hidden_states = self.ln(hidden_states)
 
-        mlp_out = self.ln_2(hidden_states)
-        mlp_out = self.mlp(mlp_out)
+            # NOTE we can contenate the input matrices of attention and MLP here for speedup
+            # but right now we avoid it since this code is only used for accuracy benchmarking at small scale
+            attention_out = self.attn(
+                hidden_states,
+                past_key_values=past_key_values,
+                attention_mask=attention_mask,
+                rope_cos_sin=rope_cos_sin,
+                cu_seqlens=cu_seqlens,
+                max_seqlen=max_seqlen,
+            )
+
+            mlp_out = self.mlp(hidden_states)
+        else:
+            attention_out = self.ln_1(hidden_states)
+            attention_out = self.attn(
+                attention_out,
+                past_key_values=past_key_values,
+                attention_mask=attention_mask,
+                rope_cos_sin=rope_cos_sin,
+                cu_seqlens=cu_seqlens,
+                max_seqlen=max_seqlen,
+            )
+
+            mlp_out = self.ln_2(hidden_states)
+            mlp_out = self.mlp(mlp_out)
 
         hidden_states = attention_out + mlp_out
 
