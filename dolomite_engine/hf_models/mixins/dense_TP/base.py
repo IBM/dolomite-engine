bb021e76ed011e0951354694979d768da37d82cb
Mayank Mishra <mayank31398@gmail.com>
2024-10-12 01:15:56 -0400

    PP
    Signed-off-by: Mayank Mishra <mayank31398@gmail.com>

--- dolomite_engine/hf_models/mixins/dense_TP/base.py
+++ dolomite_engine/hf_models/mixins/dense_TP/base.py
@@ -1,4 +1,5 @@
 import logging
+import warnings
 
 import torch
 import torch.nn as nn
@@ -174,6 +175,118 @@ class BaseModelMixin_TP(PreTrainedModelMixin_TP, BaseModelMixin):
             last_hidden_state=hidden_states,
             past_key_values=past_key_values,
             hidden_states=all_hidden_states,
+        )
+
+    def _prepare_a_bunch_of_stuff(
+        self,
+        input_ids: torch.Tensor | None = None,
+        past_key_values: DynamicCache | None = None,
+        attention_mask: torch.Tensor | None = None,
+        token_type_ids: torch.Tensor | None = None,
+        position_ids: torch.Tensor | None = None,
+        inputs_embeds: torch.Tensor | None = None,
+        use_cache: bool | None = None,
+        output_hidden_states: bool | None = None,
+        cu_seqlens: torch.Tensor | None = None,
+        max_seqlen: torch.Tensor | None = None,
+    ) -> tuple[
+        bool,
+        bool,
+        bool,
+        torch.Tensor,
+        torch.Tensor,
+        torch.Tensor,
+        torch.Tensor,
+        torch.Tensor,
+        torch.Tensor,
+        torch.Tensor,
+        tuple[torch.Tensor],
+    ]:
+        output_hidden_states = (
+            output_hidden_states if output_hidden_states is not None else self.config.output_hidden_states
+        )
+
+        if use_cache is None:
+            use_cache = False if self._use_padding_free_transformer else self.config.use_cache
+
+        if input_ids is not None and inputs_embeds is not None:
+            raise ValueError("You cannot specify both input_ids and inputs_embeds at the same time")
+        elif input_ids is not None:
+            input_shape = input_ids.size()
+
+            # special handling for padding free transformer with list inputs
+            if self._use_padding_free_transformer:
+                # for flash attention, there is no padding and we do packing
+                # so, input_ids is of shape (s1 + s2 + ... + sb)
+                batch_size = cu_seqlens.shape[0] - 1
+            else:
+                batch_size = input_shape[0]
+        elif inputs_embeds is not None:
+            # TODO special handling for padding free transformer needed here if we support inputs_embeds argument
+            input_shape = inputs_embeds.size()[:-1]
+            batch_size = input_shape[0]
+        else:
+            raise ValueError("You have to specify either input_ids or inputs_embeds")
+
+        if batch_size <= 0:
+            raise ValueError("batch_size has to be defined and > 0")
+
+        device = input_ids.device if input_ids is not None else inputs_embeds.device
+
+        if self._use_padding_free_transformer:
+            assert position_ids is not None, (
+                "GPTDolomiteModel needs position_ids from outside when using flash attention with List[List[int]] "
+                "inputs"
+            )
+        else:
+            if self.position_embedding_type == PositionEmbeddingType.alibi:
+                if position_ids is not None:
+                    warnings.warn("`position_ids` have no functionality with Alibi.", FutureWarning)
+
+            if token_type_ids is not None:
+                token_type_ids = token_type_ids.view(-1, input_shape[-1])
+
+        past_length = None
+        query_length = None
+        key_length = None
+        if self._use_padding_free_transformer:
+            key_length = max_seqlen.item()
+        else:
+            past_length = 0 if past_key_values is None else past_key_values.get_seq_length()
+            query_length = input_shape[-1]
+            key_length = past_length + query_length
+
+        if position_ids is None:
+            position_ids = self._get_position_ids(attention_mask, past_length, query_length, key_length, device)
+
+        if self.pp_world_size > 1:
+            if self.stage_id == 0:
+                hidden_states = self._get_initial_hidden_state(input_ids, inputs_embeds, position_ids, token_type_ids)
+            else:
+                hidden_states = input_ids
+        else:
+            hidden_states = self._get_initial_hidden_state(input_ids, inputs_embeds, position_ids, token_type_ids)
+
+        alibi_bias = self._get_alibi_bias(
+            attention_mask, batch_size, query_length, key_length, device, hidden_states.dtype
+        )
+
+        rope_cos_sin = self._get_rope_cos_sin(
+            key_length, position_ids, dtype=hidden_states.dtype, device=hidden_states.device
+        )
+
+        attention_mask = self._get_maybe_causal_mask(
+            attention_mask, alibi_bias, batch_size, query_length, key_length, hidden_states.dtype, device
+        )
+
+        return (
+            output_hidden_states,
+            use_cache,
+            hidden_states,
+            attention_mask,
+            position_ids,
+            rope_cos_sin,
+            past_key_values,
         )
 
     def _setup_positional_encoding(self) -> None:
