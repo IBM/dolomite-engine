082df6e4ac08c1cd8820000c5341c98a1ef60290
Mayank Mishra <mayank31398@gmail.com>
2024-10-21 16:28:05 -0400

    refactor
    Signed-off-by: Mayank Mishra <mayank31398@gmail.com>

--- dolomite_engine/hf_models/modeling_utils_TP/lm_head.py
+++ dolomite_engine/hf_models/modeling_utils_TP/lm_head.py
@@ -1,39 +1,84 @@
 import torch
 import torch.nn.functional as F
 from torch.distributed._tensor.placement_types import Replicate, Shard
+from torch.distributed.device_mesh import DeviceMesh
 
 from .embedding import Embedding_TP
-from .TP import dtensor_to_tensor, tensor_to_dtensor, use_async_tensor_parallel
+from .TP import dtensor_to_tensor, get_module_placements, tensor_to_dtensor, use_async_tensor_parallel
 
 
 class LMHead_TP(Embedding_TP):
-    def __init__(
-        self,
-        num_embeddings: int,
-        embedding_dim: int,
-        std: float | None = None,
-        tensor_parallel_word_embeddings: bool = False,
-        use_padding_free_transformer: bool = False,
-        sequence_parallel: bool = False,
-    ) -> None:
-        super().__init__(
-            num_embeddings,
-            embedding_dim,
-            std,
-            tensor_parallel_word_embeddings,
-            use_padding_free_transformer,
-            sequence_parallel,
+    def forward(self, input: torch.Tensor) -> torch.Tensor:
+        return LMHead_TP.compute_with_weight(
+            input,
+            self.weight,
+            tensor_parallel_word_embeddings=self.tensor_parallel_word_embeddings,
+            use_padding_free_transformer=self.use_padding_free_transformer,
+            sequence_parallel=self.sequence_parallel,
+            tp_mesh=self.tp_mesh,
         )
 
-        if use_async_tensor_parallel():
-            self.compile()
+    @staticmethod
+    def compute_with_weight(
+        input: torch.Tensor,
+        weight: torch.Tensor,
+        tensor_parallel_word_embeddings: bool,
+        use_padding_free_transformer: bool,
+        sequence_parallel: bool,
+        tp_mesh: DeviceMesh,
+    ) -> torch.Tensor:
+        function = (
+            torch.compile(LMHead_TP._compute_with_weight)
+            if use_async_tensor_parallel()
+            else LMHead_TP._compute_with_weight
+        )
 
-    def forward(self, input: torch.Tensor, weight: torch.Tensor | None = None) -> torch.Tensor:
-        input = tensor_to_dtensor(input, device_mesh=self.tp_mesh, current_placement=self.output_placement)
-        input = F.linear(input, self.weight if weight is None else weight)
-        input = dtensor_to_tensor(
+        input = function(
+            input=input,
+            weight=weight,
+            tensor_parallel_word_embeddings=tensor_parallel_word_embeddings,
+            use_padding_free_transformer=use_padding_free_transformer,
+            sequence_parallel=sequence_parallel,
+            tp_mesh=tp_mesh,
+        )
+
+        return input
+
+    @staticmethod
+    def _compute_with_weight(
+        input: torch.Tensor,
+        weight: torch.Tensor,
+        tensor_parallel_word_embeddings: bool,
+        use_padding_free_transformer: bool,
+        sequence_parallel: bool,
+        tp_mesh: DeviceMesh,
+    ) -> torch.Tensor:
+        input = tensor_to_dtensor(
             input,
-            device_mesh=self.tp_mesh,
-            desired_placement=Shard(-1) if self.tensor_parallel_word_embeddings else Replicate(),
+            device_mesh=tp_mesh,
+            current_placement=get_module_placements(use_padding_free_transformer, sequence_parallel),
+        )
+        input = F.linear(input, weight)
+        input = dtensor_to_tensor(
+            input, device_mesh=tp_mesh, desired_placement=Shard(-1) if tensor_parallel_word_embeddings else Replicate()
         )
         return input
+
+    @torch.compile
+    @staticmethod
+    def _compute_with_weight_compileable(
+        input: torch.Tensor,
+        weight: torch.Tensor,
+        tensor_parallel_word_embeddings: bool,
+        use_padding_free_transformer: bool,
+        sequence_parallel: bool,
+        tp_mesh: DeviceMesh,
+    ) -> torch.Tensor:
+        return LMHead_TP._compute_with_weight(
+            input=input,
+            weight=weight,
+            tensor_parallel_word_embeddings=tensor_parallel_word_embeddings,
+            use_padding_free_transformer=use_padding_free_transformer,
+            sequence_parallel=sequence_parallel,
+            tp_mesh=tp_mesh,
+        )
