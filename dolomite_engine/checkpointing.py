29828844b3b25e98a491b4a80afa5b54378fc121
Mayank Mishra <mayank31398@gmail.com>
2024-10-23 19:56:48 -0400

    container
    Signed-off-by: Mayank Mishra <mayank31398@gmail.com>

--- dolomite_engine/checkpointing.py
+++ dolomite_engine/checkpointing.py
@@ -76,7 +76,7 @@ def save_checkpoint(
     assert len(model_container) == len(optimizer_container)
     assert len(model_container) == len(lr_scheduler_container)
 
-    for pipeline_stage, model, optimizer, lr_scheduler in zip(
+    for pipeline_stage, (model, optimizer, lr_scheduler) in zip(
         model_container, optimizer_container, lr_scheduler_container
     ):
         model_state_dict = get_model_state_dict(model)
