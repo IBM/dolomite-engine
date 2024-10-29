0d28c668caded6f88b5edcfe51562ff23d65d989
Mayank Mishra <mayank31398@gmail.com>
2024-10-29 16:36:57 -0400

    tp
    Signed-off-by: Mayank Mishra <mayank31398@gmail.com>

--- dolomite_engine/hf_models/modeling_utils_TP/linear.py
+++ dolomite_engine/hf_models/modeling_utils_TP/linear.py
@@ -1,7 +1,6 @@
 import torch
 import torch.distributed
 import torch.nn as nn
-import torch.nn.functional as F
 from torch.distributed._tensor.api import DTensor
 from torch.distributed._tensor.placement_types import Partial, Replicate, Shard
 
@@ -104,29 +103,15 @@ class ColumnParallelLinear(ParameterizedLinear, DTensorModule):
 
         self.input_placement = get_module_placements(use_padding_free_transformer, sequence_parallel)
 
-        self.use_padding_free_transformer = use_padding_free_transformer
-        self.sequence_parallel = sequence_parallel
-        self.use_async_tensor_parallel = use_async_tensor_parallel()
-
-        if self.use_async_tensor_parallel:
+        if use_async_tensor_parallel():
             self.compile()
 
     def forward(self, input: torch.Tensor) -> torch.Tensor:
-        # FIXME dtensor redistribute uses alltoall for large number of GPUs
-        if self.use_async_tensor_parallel:
-            if self.sequence_parallel:
-                input = all_gather_from_sequence_parallel_region(
-                    input, dim=0 if self.use_padding_free_transformer else 1
-                )
-            else:
-                input = copy_to_tensor_parallel_region(input)
-
-            input = F.linear(input, self.weight.to_local(), None if self.bias is None else self.bias.to_local())
-        else:
-            input = tensor_to_dtensor(input, device_mesh=self.tp_mesh, current_placement=self.input_placement)
-            input = super().forward(input)
-            input = dtensor_to_tensor(input, device_mesh=self.tp_mesh, desired_placement=Shard(-1))
-
+        input = tensor_to_dtensor(
+            input, device_mesh=self.tp_mesh, current_placement=self.input_placement, desired_placement=Replicate()
+        )
+        input = super().forward(input)
+        input = dtensor_to_tensor(input, device_mesh=self.tp_mesh, desired_placement=Shard(-1))
         return input
 
     def extra_repr(self) -> str:
