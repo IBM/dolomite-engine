8cb223645de9cd2168af6df3e38c6d72a8633ba7
Mayank Mishra <mayank31398@gmail.com>
2024-11-18 12:21:41 -0500

    blocks
    Signed-off-by: Mayank Mishra <mayank31398@gmail.com>

--- dolomite_engine/data/dataloader.py
+++ dolomite_engine/data/dataloader.py
@@ -10,48 +10,6 @@ from ..utils import ProcessGroupManager
 
 
 class ResumableDataLoader(DataLoader):
-    def __init__(
-        self,
-        dataset: Dataset,
-        batch_size: int = 1,
-        shuffle: bool | None = None,
-        sampler: Sampler | Iterable | None = None,
-        batch_sampler: Sampler[list] | Iterable[list] | None = None,
-        num_workers: int = 0,
-        collate_fn: Callable | None = None,
-        pin_memory: bool = False,
-        drop_last: bool = False,
-        timeout: float = 0,
-        worker_init_fn: Callable | None = None,
-        multiprocessing_context=None,
-        generator=None,
-        *,
-        prefetch_factor: int | None = None,
-        persistent_workers: bool = False,
-        pin_memory_device: str = "",
-        static_loss_scaler: int | None = None,
-    ) -> None:
-        super().__init__(
-            dataset,
-            batch_size,
-            shuffle,
-            sampler,
-            batch_sampler,
-            num_workers,
-            collate_fn,
-            pin_memory,
-            drop_last,
-            timeout,
-            worker_init_fn,
-            multiprocessing_context,
-            generator,
-            prefetch_factor=prefetch_factor,
-            persistent_workers=persistent_workers,
-            pin_memory_device=pin_memory_device,
-        )
-
-        self.static_loss_scaler = static_loss_scaler
-
     def get_loss_scaler(self) -> int:
         if self.static_loss_scaler is None:
             raise NotImplementedError()
