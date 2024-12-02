b47051921da838ef6b016bba68e5fa42a7b947f0
Mayank Mishra <mayank31398@gmail.com>
2024-12-01 22:44:44 -0500

    ladder TP test
    Signed-off-by: Mayank Mishra <mayank31398@gmail.com>

--- dolomite_engine/hf_models/models/ladder_residual_TP/main.py
+++ dolomite_engine/hf_models/models/ladder_residual_TP/main.py
@@ -1,6 +1,8 @@
 from ...mixins import CausalLMModelMixin_TP
+from ..gpt_dolomite_TP.weights import get_gpt_dolomite_model_parallel_state_dict
 from .base import LadderResidualModel_TP, LadderResidualPreTrainedModel_TP
 
 
 class LadderResidualForCausalLM_TP(LadderResidualPreTrainedModel_TP, CausalLMModelMixin_TP):
     base_model_class = LadderResidualModel_TP
+    model_parallel_state_dict_function = get_gpt_dolomite_model_parallel_state_dict
