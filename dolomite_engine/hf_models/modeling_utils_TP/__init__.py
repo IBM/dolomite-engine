5f782663cc6509f630b09aae6470e11f8b3532f1
Mayank Mishra <mayank31398@gmail.com>
2024-12-06 13:50:04 -0500

    compile
    Signed-off-by: Mayank Mishra <mayank31398@gmail.com>

--- dolomite_engine/hf_models/modeling_utils_TP/__init__.py
+++ dolomite_engine/hf_models/modeling_utils_TP/__init__.py
@@ -2,7 +2,7 @@ from .attention import Attention_TP, get_attention_module_TP
 from .dropout import Dropout_TP
 from .dtensor_module import DTensorModule
 from .embedding import Embedding_TP, get_tensor_parallel_vocab_info
-from .linear import ColumnParallelLinear, Linear_TP, ReplicatedLinear, RowParallelLinear
+from .linear import ColumnParallelLinear, ReplicatedLinear, RowParallelLinear
 from .lm_head import LMHead_TP
 from .normalization import get_normalization_function_TP
 from .position_embedding import Alibi_TP
