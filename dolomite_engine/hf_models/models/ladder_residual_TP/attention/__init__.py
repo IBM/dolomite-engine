34e22df5e340b0c7ca1e0d59b9ee4a32bb6f14b5
Mayank Mishra <mayank31398@gmail.com>
2024-12-07 16:32:37 -0500

    add attention
    Signed-off-by: Mayank Mishra <mayank31398@gmail.com>

--- dolomite_engine/hf_models/models/ladder_residual_TP/attention/__init__.py
+++ dolomite_engine/hf_models/models/ladder_residual_TP/attention/__init__.py
@@ -1,10 +1,5 @@
 from ...ladder_residual import LadderResidualConfig
-from .flash import LadderResidualFlashAttention2_TP
-from .padding_free import PaddingFreeAttention_TP
-from .sdpa import LadderResidualSDPA_TP
-
-
-_ATTENTION_MODULES = {"sdpa": LadderResidualSDPA_TP, "flash_attention_2": LadderResidualFlashAttention2_TP}
+from .padding_free import LadderResidualPaddingFreeAttention_TP
 
 
 def get_attention_module_TP(
@@ -14,13 +9,13 @@ def get_attention_module_TP(
     use_padding_free_transformer: bool,
     layer_idx: int,
     sequence_parallel: bool,
-) -> LadderResidualSDPA_TP | LadderResidualFlashAttention2_TP:
+) -> LadderResidualPaddingFreeAttention_TP:
+    assert use_padding_free_transformer
+
     if use_padding_free_transformer:
         assert (
             attention_implementation == "flash_attention_2"
         ), "padding free transformer only works with flash attention"
-        attention_class = PaddingFreeAttention_TP
-    else:
-        attention_class = _ATTENTION_MODULES[attention_implementation]
+        attention_class = LadderResidualPaddingFreeAttention_TP
 
     return attention_class(config, causal=causal, layer_idx=layer_idx, sequence_parallel=sequence_parallel)
