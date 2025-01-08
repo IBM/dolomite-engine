4845c06a9d5037e6194463c899f29e2534d13d8e
Mayank Mishra <mayank31398@gmail.com>
2025-01-08 18:37:42 +0530

    cleanup
    Signed-off-by: Mayank Mishra <mayank31398@gmail.com>

--- dolomite_engine/finetune.py
+++ dolomite_engine/finetune.py
@@ -274,13 +274,15 @@ def evaluate(
 
     metrics_tracker = MetricsTrackingDict({})
     val_dataloader = custom_iterator(val_dataloader, infinite=False)
+    loss_tokens = 0
 
     for _ in range(num_steps):
         batch = get_next_batch(val_dataloader)
+        loss_tokens += (batch["labels"] != -100).sum()
         loss_step_dict = model_container[0](batch)
         metrics_tracker = metrics_tracker + loss_step_dict
 
-    metrics_tracker = metrics_tracker / num_steps
+    metrics_tracker = metrics_tracker / loss_tokens
 
     for key in metrics_tracker:
         metrics_tracker[key] = dtensor_to_tensor(metrics_tracker[key])
