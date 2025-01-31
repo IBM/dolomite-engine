b1c821877ba3aef28ffdcf1ef4d99661b3f4b5ce
Mayank Mishra <mayank31398@gmail.com>
2025-01-31 13:03:50 -0500

    cleanup
    Signed-off-by: Mayank Mishra <mayank31398@gmail.com>

--- dolomite_engine/hf_models/models/__init__.py
+++ dolomite_engine/hf_models/models/__init__.py
@@ -20,6 +20,7 @@ from .gpt_dolomite_TP import (
 )
 from .ladder_residual import LadderResidualConfig, LadderResidualForCausalLM, LadderResidualModel
 from .ladder_residual_TP import LadderResidualForCausalLM_TP, LadderResidualModel_TP
+from .mamba2_dolomite import Mamba2DolomiteConfig, Mamba2DolomiteForCausalLM, Mamba2DolomiteModel
 from .moe_dolomite import MoEDolomiteConfig, MoEDolomiteForCausalLM, MoEDolomiteModel
 from .moe_dolomite_TP import (
     MoEDolomiteForCausalLM_TP,
