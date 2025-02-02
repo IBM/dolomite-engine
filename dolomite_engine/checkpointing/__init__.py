fd030594f282b0395f509d4bbc55415ea45ec586
Mayank Mishra <mayank31398@gmail.com>
2025-02-02 11:14:27 -0500

    cleanup
    Signed-off-by: Mayank Mishra <mayank31398@gmail.com>

--- dolomite_engine/checkpointing/__init__.py
+++ dolomite_engine/checkpointing/__init__.py
@@ -22,14 +22,15 @@ from torch.distributed.checkpoint.stateful import Stateful
 from torch.optim import Optimizer
 from torch.optim.lr_scheduler import LambdaLR
 
-from .arguments import InferenceArgs, TrainingArgs, UnshardingArgs
-from .containers import LRSchedulerContainer, ModelContainer, OptimizerContainer
-from .data import ResumableDataLoader
-from .enums import Mode
-from .hf_models import fix_unsharded_state_dict
-from .model_wrapper import ModelWrapper, get_model_container
-from .optimization import get_scheduler_container
-from .utils import ExperimentsTracker, ProcessGroupManager, load_yaml, log_rank_0, run_rank_n, string_to_torch_dtype
+from ..arguments import InferenceArgs, TrainingArgs, UnshardingArgs
+from ..containers import LRSchedulerContainer, ModelContainer, OptimizerContainer
+from ..data import ResumableDataLoader
+from ..enums import Mode
+from ..hf_models import fix_unsharded_state_dict
+from ..model_wrapper import ModelWrapper, get_model_container
+from ..optimization import get_scheduler_container
+from ..utils import ExperimentsTracker, ProcessGroupManager, load_yaml, log_rank_0, run_rank_n, string_to_torch_dtype
+from .lr_scheduler import _get_lr_scheduler_path, _LRSchedulerSaver, _resume_learning_rate
 
 
 _TRAINING_CONFIG_PREFIX = "training_config"
@@ -104,20 +105,6 @@ class _Saver(Stateful):
         self._load_model_state_dict(state_dict["model"])
         if self.optimizer_container is not None:
             self._load_optimizer_state_dict(state_dict["optimizer"])
-
-
-class _LRSchedulerSaver(Stateful):
-    def __init__(self, lr_scheduler_container: LRSchedulerContainer) -> None:
-        self.lr_scheduler_container = lr_scheduler_container
-
-    def state_dict(self) -> dict:
-        return [lr_scheduler.state_dict() for lr_scheduler in self.lr_scheduler_container]
-
-    def load_state_dict(self, state_dict: list[dict]) -> None:
-        assert len(self.lr_scheduler_container) == len(state_dict)
-
-        for lr_scheduler, lr_scheduler_state_dict in zip(self.lr_scheduler_container, state_dict):
-            lr_scheduler.load_state_dict(lr_scheduler_state_dict)
 
 
 def save_checkpoint(
@@ -396,35 +383,6 @@ def save_args(args: TrainingArgs | InferenceArgs, save_path: str, mode: Mode) ->
     yaml.dump(args.to_dict(), open(save_path, "w"), indent=2)
 
 
-def _resume_learning_rate(
-    args: TrainingArgs, optimizer: Optimizer, lr_scheduler: LambdaLR, iteration: int | None = None
-) -> None:
-    initial_lr = []
-    for grp in optimizer.param_groups:
-        initial_lr.append(grp["initial_lr"])
-        grp["initial_lr"] = grp["lr"]
-
-    # we create lr scheduler again here since optimizer is loaded from disk and lr scheduler is now out of sync
-    # this helps to resume phase 2
-    lr_scheduler_tmp = get_scheduler_container(
-        optimizer_container=OptimizerContainer([optimizer]),
-        num_warmup_steps=args.lr_scheduler_args.num_warmup_steps,
-        num_constant_steps=args.lr_scheduler_args.num_constant_steps,
-        num_decay_steps=args.lr_scheduler_args.num_decay_steps,
-        num_training_steps=args.training_parameters.num_training_steps,
-        lr_decay_style=args.lr_scheduler_args.lr_decay_style,
-        lr_decay_factor=args.lr_scheduler_args.lr_decay_factor,
-        extra_lr_scheduler_args=args.lr_scheduler_args.extra_lr_scheduler_args,
-        last_epoch=-1 if iteration is None else iteration - 1,
-    )[0]
-
-    for grp, lr_ in zip(optimizer.param_groups, initial_lr):
-        grp["initial_lr"] = lr_
-
-    lr_scheduler.load_state_dict(lr_scheduler_tmp.state_dict())
-    del lr_scheduler_tmp
-
-
 def _get_checkpoint_tag(iteration: int) -> str:
     return f"global_step{iteration}"
 
@@ -435,10 +393,6 @@ def _get_base_path(path: str, iteration: int) -> str:
 
 def _get_model_optimizer_path(path: str) -> str:
     return os.path.join(path, "model_optimizer")
-
-
-def _get_lr_scheduler_path(path: str) -> str:
-    return os.path.join(path, "lr_scheduler", f"lr_scheduler-{ProcessGroupManager.get_global_rank()}.pt")
 
 
 def _get_dataloader_path(path: str) -> str:
