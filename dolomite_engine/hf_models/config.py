02d8965da7eaa362d452f49f99e19a802e749943
Mayank Mishra <mayank31398@gmail.com>
2025-02-05 11:13:39 -0500

    cleanup
    Signed-off-by: Mayank Mishra <mayank31398@gmail.com>

--- dolomite_engine/hf_models/config.py
+++ dolomite_engine/hf_models/config.py
@@ -46,6 +46,11 @@ class CommonConfig(PretrainedConfig):
         init_method: str = "normal",
         upcast_logits_for_loss: bool = False,
         mlp_blocks: list[str] = None,
+        num_experts: int = 8,
+        num_experts_per_tok: int = 2,
+        router_aux_loss_coef: float = 0.001,
+        shared_n_inner: int | None = None,
+        use_aux_free_moe: bool = False,
         **kwargs,
     ) -> None:
         self.vocab_size = vocab_size
@@ -112,5 +117,14 @@ class CommonConfig(PretrainedConfig):
         self.mlp_blocks = mlp_blocks
         if self.mlp_blocks is None:
             self.mlp_blocks = [{"mlp_block_type": "MLP"} for _ in range(self.n_layer)]
+
+        self.num_experts = num_experts
+        self.num_experts_per_tok = num_experts_per_tok
+
+        self.shared_n_inner = shared_n_inner
+
+        self.router_aux_loss_coef = router_aux_loss_coef
+
+        self.use_aux_free_moe = use_aux_free_moe
 
         super().__init__(bos_token_id=bos_token_id, eos_token_id=eos_token_id, pad_token_id=pad_token_id, **kwargs)
