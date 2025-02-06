d443decacd97a55439e63c316ecde3f9f00b45d5
Mayank Mishra <mayank31398@gmail.com>
2025-02-06 18:19:34 -0500

    cleanup
    Signed-off-by: Mayank Mishra <mayank31398@gmail.com>

--- tests/hf_models/multi_gpu/unsharding/unsharding.py
+++ tests/hf_models/multi_gpu/unsharding/unsharding.py
@@ -55,7 +55,6 @@ if is_tp_first_rank:
 torch.distributed.barrier()
 
 model_tp = get_model_parallel_class(config.model_type).from_pretrained(args.tmp_path)
-
 tp_state_dict = model_tp.state_dict()
 
 
