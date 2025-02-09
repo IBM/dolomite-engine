a1da808c16139611137d8e4e2d64e8c13aff281d
Mayank Mishra <mayank31398@gmail.com>
2025-02-09 15:58:51 -0500

    cleanup
    Signed-off-by: Mayank Mishra <mayank31398@gmail.com>

--- dolomite_engine/hf_models/models/gpt_dolomite_TP/weights/fix.py
+++ dolomite_engine/hf_models/models/gpt_dolomite_TP/weights/fix.py
@@ -13,7 +13,10 @@ def fix_gpt_dolomite_unsharded_state_dict(
     ]
 
     for layer_idx in range(config.num_layers):
-        if AttentionHeadType(config.attention_head_type) == AttentionHeadType.mqa:
+        if (
+            config.check_equal_for_all_and_get_value("sequence_mixer_blocks", "attention_head_type")
+            == AttentionHeadType.mqa
+        ):
             q_attn_w = state_dict.pop(f"{prefix}transformer.h.{layer_idx}.sequence_mixer.c_attn.q_attn.weight")
             kv_attn_w = state_dict.pop(f"{prefix}transformer.h.{layer_idx}.sequence_mixer.c_attn.kv_attn.weight")
             state_dict[f"{prefix}transformer.h.{layer_idx}.sequence_mixer.c_attn.weight"] = torch.cat(
