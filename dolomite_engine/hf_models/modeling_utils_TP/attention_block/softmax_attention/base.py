67aca4a011729afae7fc0c0f0e3fa25e4b4de0d3
Mayank Mishra <mayank31398@gmail.com>
2025-02-09 15:47:00 -0500

    cleanup
    Signed-off-by: Mayank Mishra <mayank31398@gmail.com>

--- dolomite_engine/hf_models/modeling_utils_TP/attention_block/softmax_attention/base.py
+++ dolomite_engine/hf_models/modeling_utils_TP/attention_block/softmax_attention/base.py
@@ -5,7 +5,6 @@ import torch.distributed
 import torch.nn as nn
 
 from .....utils import ProcessGroupManager, divide_if_divisible
-from ....config import CommonConfig
 from ....enums import AttentionHeadType, InitMethod, PositionEmbeddingType
 from ....modeling_utils import Attention
 from ....modeling_utils.mlp_block.mlp import _get_std_for_linear
@@ -15,6 +14,45 @@ from ...linear import ColumnParallelLinear, ReplicatedLinear, RowParallelLinear
 
 class _BaseAttention_TP(nn.Module):
     def __init__(
+        self,
+        hidden_size: int,
+        num_attention_heads: int,
+        num_key_value_heads: int,
+        attention_multiplier: float,
+        attention_head_type: AttentionHeadType,
+        position_embedding_type: PositionEmbeddingType,
+        add_bias: bool,
+        softmax_dropout: float,
+        dropout: float,
+        init_method: InitMethod,
+        initializer_range: float,
+        m_width: float,
+        num_layers: int,
+        causal: bool,
+        layer_idx: int | None = None,
+        sequence_parallel: bool = False,
+    ) -> None:
+        self._init_attention(
+            hidden_size=hidden_size,
+            num_attention_heads=num_attention_heads,
+            num_key_value_heads=num_key_value_heads,
+            attention_multiplier=attention_multiplier,
+            attention_head_type=attention_head_type,
+            position_embedding_type=position_embedding_type,
+            add_bias=add_bias,
+            softmax_dropout=softmax_dropout,
+            dropout=dropout,
+            init_method=init_method,
+            initializer_range=initializer_range,
+            m_width=m_width,
+            num_layers=num_layers,
+            causal=causal,
+            layer_idx=layer_idx,
+            use_padding_free_transformer=False,
+            sequence_parallel=sequence_parallel,
+        )
+
+    def _init_attention(
         self,
         hidden_size: int,
         num_attention_heads: int,
@@ -242,15 +280,38 @@ class _MQA_QueryKeyValueProjection(nn.Module):
 class Attention_TP(_BaseAttention_TP, Attention):
     def __init__(
         self,
-        config: CommonConfig,
+        hidden_size: int,
+        num_attention_heads: int,
+        num_key_value_heads: int,
+        attention_multiplier: float,
+        attention_head_type: AttentionHeadType,
+        position_embedding_type: PositionEmbeddingType,
+        add_bias: bool,
+        softmax_dropout: float,
+        dropout: float,
+        init_method: InitMethod,
+        initializer_range: float,
+        m_width: float,
+        num_layers: int,
         causal: bool,
         layer_idx: int | None = None,
         sequence_parallel: bool = False,
     ) -> None:
-        _BaseAttention_TP.__init__(
-            self,
-            config,
-            causal,
+        self._init_attention(
+            hidden_size=hidden_size,
+            num_attention_heads=num_attention_heads,
+            num_key_value_heads=num_key_value_heads,
+            attention_multiplier=attention_multiplier,
+            attention_head_type=attention_head_type,
+            position_embedding_type=position_embedding_type,
+            add_bias=add_bias,
+            softmax_dropout=softmax_dropout,
+            dropout=dropout,
+            init_method=init_method,
+            initializer_range=initializer_range,
+            m_width=m_width,
+            num_layers=num_layers,
+            causal=causal,
             layer_idx=layer_idx,
             use_padding_free_transformer=False,
             sequence_parallel=sequence_parallel,
