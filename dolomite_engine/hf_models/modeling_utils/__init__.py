83564e6d2d5e7e6565950425078b31626227c7f9
Mayank Mishra <mayank31398@gmail.com>
2025-02-10 00:05:05 -0500

    merge
    Signed-off-by: Mayank Mishra <mayank31398@gmail.com>

--- dolomite_engine/hf_models/modeling_utils/__init__.py
+++ dolomite_engine/hf_models/modeling_utils/__init__.py
@@ -1,14 +1,4 @@
 from .activations import get_activation_function, is_glu
-from .attention_block import (
-    SDPA,
-    Attention,
-    FlashAttention2,
-    PaddingFreeAttention,
-    get_sequence_mixer,
-    interleave_query_key_value_tensor_for_attention,
-    repeat_key_value,
-    split_query_key_value_tensor_for_attention,
-)
 from .embedding import ParameterizedEmbedding
 from .linear import ParameterizedLinear
 from .mlp_blocks import (
@@ -23,3 +13,13 @@ from .mlp_blocks import (
 )
 from .normalization import get_normalization_function
 from .position_embedding import RoPE, YaRNScaledRoPE, apply_rotary_pos_emb
+from .sequence_mixer_blocks import (
+    SDPA,
+    Attention,
+    FlashAttention2,
+    PaddingFreeAttention,
+    get_sequence_mixer,
+    interleave_query_key_value_tensor_for_attention,
+    repeat_key_value,
+    split_query_key_value_tensor_for_attention,
+)
--- dolomite_engine/hf_models/modeling_utils_TP/__init__.py
+++ dolomite_engine/hf_models/modeling_utils_TP/__init__.py
@@ -1,4 +1,3 @@
-from .attention_block import Attention_TP, get_sequence_mixer_TP
 from .dropout import Dropout_TP
 from .dtensor_module import DTensorModule
 from .embedding import Embedding_TP, get_tensor_parallel_vocab_info
@@ -6,4 +5,5 @@ from .linear import ColumnParallelLinear, ReplicatedLinear, RowParallelLinear
 from .lm_head import LMHead_TP
 from .mlp_blocks import MLP_TP, ScatterMoE_TP, get_mlp_block_TP
 from .normalization import get_normalization_function_TP
+from .sequence_mixer_blocks import Attention_TP, get_sequence_mixer_TP
 from .TP import get_module_placements, tensor_parallel_split_safetensor_slice
