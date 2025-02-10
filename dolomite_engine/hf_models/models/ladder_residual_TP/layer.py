fb8c0b29b9dcf3d600a111d3a4719695ca22a9c5
Mayank Mishra <mayank.mishra2@ibm.com>
2025-02-10 22:30:22 +0000

    init

--- dolomite_engine/hf_models/models/ladder_residual_TP/layer.py
+++ dolomite_engine/hf_models/models/ladder_residual_TP/layer.py
@@ -2,6 +2,7 @@ import torch
 from transformers import DynamicCache
 
 from ..gpt_dolomite_TP.layer import GPTDolomiteBlock_TP
+from .rmsnorm import rmsnorm_cute_forward_only, rmsnorm_cute_backward_only
 
 
 class LadderResidualBlock_TP(GPTDolomiteBlock_TP):
@@ -19,7 +20,9 @@ class LadderResidualBlock_TP(GPTDolomiteBlock_TP):
         if current_attention_out is not None:
             residual = residual + current_attention_out
 
-        current_attention_out = self.ln_1(residual)
+        ln_1_weight = self.ln_1.weight
+        current_attention_out, current_attention_x, ln_1_weight, current_attention_rmsnorm_denominator, attention_is_x_1d = rmsnorm_cute_forward_only(residual, ln_1_weight, self.ln_1.eps, False)
+
         current_attention_out = self.sequence_mixer(
             current_attention_out,
             past_key_values=past_key_values,
@@ -35,7 +38,9 @@ class LadderResidualBlock_TP(GPTDolomiteBlock_TP):
         if current_mlp_out is not None:
             residual = residual + current_mlp_out
 
-        current_mlp_out = self.ln_2(residual)
+        ln_2_weight = self.ln_2.weight
+        current_mlp_out, current_mlp_x, ln_2_weight, current_mlp_rmsnorm_denominator, mlp_is_x_1d = rmsnorm_cute_forward_only(residual, ln_2_weight, self.ln_2.eps, False)
+
         current_mlp_out = self.mlp_block(current_mlp_out)
 
         if self.m_residual is not None:
---
+++ dolomite_engine/hf_models/models/ladder_residual_TP/rmsnorm.py
@@ -0,0 +1,124 @@
+import torch
+from cute_kernels.cutotune import CutoTuneParameter
+from cute_kernels.enums import KernelBackend
+from cute_kernels.utils import ensure_contiguous
+from cute_kernels.kernels.rmsnorm.backward import _backward
+from cute_kernels.kernels.rmsnorm.forward import _forward
+
+
+class _RMSNorm_Cute_Forward(torch.autograd.Function):
+    @staticmethod
+    @ensure_contiguous
+    def forward(
+        ctx,
+        x: torch.Tensor,
+        weight: torch.Tensor | None,
+        eps: float | None,
+        memory_efficient: bool,
+        kernel_backend_forward: KernelBackend,
+        BLOCK_SIZE_B_forward: int,
+        BLOCK_SIZE_H_forward: int,
+    ) -> tuple[torch.Tensor, torch.Tensor, torch.Tensor | None, torch.Tensor | None, bool]:
+        if weight is not None:
+            assert weight.dim() == 1, "weight should be 1D"
+            assert weight.size(-1) == x.size(-1), "hidden size for x and weight tensor is different"
+            assert weight.type() == x.type(), "tensors weight and y should have same dtype"
+
+        is_x_1d = x.dim() == 1
+        if is_x_1d:
+            x = x.unsqueeze(0)
+
+        if eps is None:
+            eps = torch.finfo(x.dtype).eps
+
+        output, rmsnorm_denominator = _forward(
+            x=x,
+            weight=weight,
+            eps=eps,
+            memory_efficient=memory_efficient,
+            kernel_backend=kernel_backend_forward,
+            BLOCK_SIZE_B=BLOCK_SIZE_B_forward,
+            BLOCK_SIZE_H=BLOCK_SIZE_H_forward,
+        )
+
+        if is_x_1d:
+            output = output.squeeze(0)
+
+        return output, x, weight, rmsnorm_denominator, is_x_1d
+
+    @staticmethod
+    @ensure_contiguous
+    def backward(ctx, output_grad: torch.Tensor, x_grad: torch.Tensor, weight_grad: torch.Tensor | None, rmsnorm_denominator: torch.Tensor | None, is_x_1d_grad: bool) -> tuple[torch.Tensor | None]:
+        return x_grad, weight_grad, *[None] * 5
+
+
+class _RMSNorm_Cute_Backward(torch.autograd.Function):
+    @staticmethod
+    @ensure_contiguous
+    def forward(
+        ctx,
+        x: torch.Tensor,
+        weight: torch.Tensor | None,
+        output: torch.Tensor,
+        rmsnorm_denominator: torch.Tensor | None,
+        eps: float | None,
+        kernel_backend_backward: KernelBackend,
+        BLOCK_SIZE_B_backward: int,
+        BLOCK_SIZE_H_backward: int,
+        is_x_1d: bool,
+    ) -> torch.Tensor:
+        ctx.save_for_backward(x, weight, rmsnorm_denominator)
+        ctx.eps = eps
+        ctx.is_x_1d = is_x_1d
+        ctx.kernel_backend_backward = kernel_backend_backward
+        ctx.BLOCK_SIZE_B_backward = BLOCK_SIZE_B_backward
+        ctx.BLOCK_SIZE_H_backward = BLOCK_SIZE_H_backward
+
+        return output
+
+    @staticmethod
+    @ensure_contiguous
+    def backward(ctx, output_grad: torch.Tensor) -> tuple[torch.Tensor | None]:
+        x, weight, rmsnorm_denominator = ctx.saved_tensors
+
+        x_grad, weight_grad = _backward(
+            x=x,
+            weight=weight,
+            eps=ctx.eps,
+            rmsnorm_denominator=rmsnorm_denominator,
+            output_grad=output_grad,
+            kernel_backend=ctx.kernel_backend_backward,
+            BLOCK_SIZE_B=ctx.BLOCK_SIZE_B_backward,
+            BLOCK_SIZE_H=ctx.BLOCK_SIZE_H_backward,
+        )
+
+        if ctx.is_x_1d:
+            x_grad = x_grad.squeeze(0)
+
+        return x_grad, weight_grad, *[None] * 7
+
+
+def rmsnorm_cute_forward_only(
+    x: torch.Tensor,
+    weight: torch.Tensor | None,
+    eps: float | None,
+    memory_efficient: bool = False,
+    kernel_backend_forward: KernelBackend = CutoTuneParameter(),
+    BLOCK_SIZE_B_forward: int = CutoTuneParameter(),
+    BLOCK_SIZE_H_forward: int = CutoTuneParameter(),
+) -> torch.Tensor:
+    return _RMSNorm_Cute_Forward.apply(x, weight, eps, memory_efficient, kernel_backend_forward, BLOCK_SIZE_B_forward, BLOCK_SIZE_H_forward)
+
+
+def rmsnorm_cute_backward_only(
+    x: torch.Tensor,
+    weight: torch.Tensor | None,
+    output: torch.Tensor,
+    rmsnorm_denominator: torch.Tensor | None,
+    eps: float | None,
+    kernel_backend_backward: KernelBackend = CutoTuneParameter(),
+    BLOCK_SIZE_B_backward: int = CutoTuneParameter(),
+    BLOCK_SIZE_H_backward: int = CutoTuneParameter(),
+    is_x_1d: bool = False,
+) -> torch.Tensor:
+    return _RMSNorm_Cute_Backward.apply(x, weight, output, rmsnorm_denominator, eps, kernel_backend_backward, BLOCK_SIZE_B_backward, BLOCK_SIZE_H_backward, is_x_1d)
