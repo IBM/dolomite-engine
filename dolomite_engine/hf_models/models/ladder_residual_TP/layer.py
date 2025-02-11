090feec346f0e51e0eb766f2efb4e099ab9394d8
Mayank Mishra <mayank31398@gmail.com>
2025-02-10 20:42:46 -0500

    merge
    Signed-off-by: Mayank Mishra <mayank31398@gmail.com>

--- dolomite_engine/hf_models/models/ladder_residual_TP/layer.py
+++ dolomite_engine/hf_models/models/ladder_residual_TP/layer.py
@@ -1,13 +1,35 @@
 import torch
 from cute_kernels import rmsnorm_cute
+from torch.distributed._tensor.placement_types import Partial, Replicate
 from transformers import DynamicCache
 
+from ....distributed import dtensor_to_tensor
 from ....kernels import wait_for_ACT
 from ..gpt_dolomite_TP.layer import GPTDolomiteBlock_TP
 from .rmsnorm import rmsnorm_cute_backward_only, rmsnorm_cute_forward_only
 
 
+def rmsnorm_cute_wrapper(
+    input: torch.Tensor, weight: torch.Tensor, eps: float, sequence_parallel: bool
+) -> torch.Tensor:
+    input = wait_for_ACT(input, wait_in_forward=True, wait_in_backward=False)
+    input = rmsnorm_cute(
+        x=input,
+        weight=dtensor_to_tensor(weight, grad_placement=Partial() if sequence_parallel else Replicate()),
+        eps=eps,
+        memory_efficient=False,
+    )
+    input = wait_for_ACT(input, wait_in_forward=False, wait_in_backward=True)
+    return input
+
+
 class LadderResidualBlock_TP(GPTDolomiteBlock_TP):
+    def __init__(
+        self, config, attention_implementation, use_padding_free_transformer, layer_idx=None, sequence_parallel=False
+    ):
+        super().__init__(config, attention_implementation, use_padding_free_transformer, layer_idx, sequence_parallel)
+        self.sequence_parallel = sequence_parallel
+
     def forward(
         self,
         current_attention_out: torch.Tensor,
@@ -22,13 +44,7 @@ class LadderResidualBlock_TP(GPTDolomiteBlock_TP):
         if current_attention_out is not None:
             residual = residual + current_attention_out
 
-        current_attention_out = rmsnorm_cute(
-            x=wait_for_ACT(residual, wait_in_forward=True, wait_in_backward=False),
-            weight=wait_for_ACT(self.ln_1.weight, wait_in_forward=True, wait_in_backward=False),
-            eps=self.ln_1.eps,
-        )
-        current_attention_out = wait_for_ACT(current_attention_out, wait_in_forward=False, wait_in_backward=True)
-
+        current_attention_out = rmsnorm_cute_wrapper(residual, self.ln_1.weight, self.ln_1.eps, self.sequence_parallel)
         current_attention_out = self.sequence_mixer(
             current_attention_out,
             past_key_values=past_key_values,
@@ -44,11 +60,7 @@ class LadderResidualBlock_TP(GPTDolomiteBlock_TP):
         if current_mlp_out is not None:
             residual = residual + current_mlp_out
 
-        current_mlp_out = rmsnorm_cute(
-            x=wait_for_ACT(residual, wait_in_forward=True, wait_in_backward=False),
-            weight=wait_for_ACT(self.ln_2.weight, wait_in_forward=True, wait_in_backward=False),
-            eps=self.ln_1.eps,
-        )
+        current_mlp_out = rmsnorm_cute_wrapper(residual, self.ln_2.weight, self.ln_2.eps, self.sequence_parallel)
         current_mlp_out = wait_for_ACT(current_mlp_out, wait_in_forward=False, wait_in_backward=True)
 
         current_mlp_out = self.mlp_block(current_mlp_out)
