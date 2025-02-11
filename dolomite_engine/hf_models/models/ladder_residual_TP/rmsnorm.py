b4ce61b174c45d96b050be3da4d681e55e1be0b2
Mayank Mishra <mayank31398@gmail.com>
2025-02-10 22:17:40 -0500

    merge
    Signed-off-by: Mayank Mishra <mayank31398@gmail.com>

--- dolomite_engine/hf_models/models/ladder_residual_TP/rmsnorm.py
+++ dolomite_engine/hf_models/models/ladder_residual_TP/rmsnorm.py
@@ -5,7 +5,7 @@ from cute_kernels.kernels.rmsnorm.forward import _forward
 from cute_kernels.utils import ensure_contiguous
 from torch.distributed._tensor.placement_types import Partial, Replicate
 
-from ....distributed import dtensor_to_tensor
+from ....distributed import dtensor_to_tensor, tensor_to_dtensor
 from ....kernels import wait_for_ACT
 
 
@@ -16,8 +16,13 @@ _MLP_B = []
 class _RMSNorm_Cute_F(torch.autograd.Function):
     @staticmethod
     @ensure_contiguous
-    def forward(ctx, x: torch.Tensor, weight: torch.Tensor | None, eps: float | None) -> torch.Tensor:
+    def forward(
+        ctx, x: torch.Tensor, weight: torch.Tensor | None, eps: float | None, sequence_parallel: bool
+    ) -> torch.Tensor:
         if weight is not None:
+            device_mesh = weight.device_mesh
+            weight = dtensor_to_tensor(weight)
+
             assert weight.dim() == 1, "weight should be 1D"
             assert weight.size(-1) == x.size(-1), "hidden size for x and weight tensor is different"
             assert weight.type() == x.type(), "tensors weight and y should have same dtype"
@@ -41,14 +46,14 @@ class _RMSNorm_Cute_F(torch.autograd.Function):
         if is_x_1d:
             output = output.squeeze(0)
 
-        _MLP_F.append((x, weight, rmsnorm_denominator, is_x_1d, eps))
+        _MLP_F.append((x, weight, rmsnorm_denominator, is_x_1d, eps, sequence_parallel, device_mesh))
 
         return output
 
     @staticmethod
     def backward(ctx, output_grad: torch.Tensor) -> tuple[torch.Tensor | None]:
         _MLP_B.append(output_grad)
-        return None, None, None
+        return None, None, None, None
 
 
 class _RMSNorm_Cute_B(torch.autograd.Function):
@@ -59,7 +64,7 @@ class _RMSNorm_Cute_B(torch.autograd.Function):
     @staticmethod
     @ensure_contiguous
     def backward(ctx, x_grad: torch.Tensor, weight_grad: torch.Tensor) -> tuple[torch.Tensor | None]:
-        x, weight, rmsnorm_denominator, is_x_1d, eps = _MLP_F.pop()
+        x, weight, rmsnorm_denominator, is_x_1d, eps, sequence_parallel, device_mesh = _MLP_F.pop()
         output_grad = _MLP_B.pop()
 
         output_grad = output_grad.contiguous()
@@ -81,6 +86,10 @@ class _RMSNorm_Cute_B(torch.autograd.Function):
         if is_x_1d:
             x_grad = x_grad.squeeze(0)
 
+        weight_grad = tensor_to_dtensor(
+            weight_grad, device_mesh=device_mesh, current_placement=Partial() if sequence_parallel else Replicate()
+        )
+
         return x_grad, weight_grad, None
 
 
@@ -88,9 +97,7 @@ def rmsnorm_cute_forward(
     x: torch.Tensor, weight: torch.Tensor | None, eps: float | None, sequence_parallel: bool
 ) -> torch.Tensor:
     x = wait_for_ACT(x, wait_in_forward=True, wait_in_backward=False)
-    x = _RMSNorm_Cute_F.apply(
-        x, dtensor_to_tensor(weight, grad_placement=Partial() if sequence_parallel else Replicate()), eps
-    )
+    x = _RMSNorm_Cute_F.apply(x, weight, eps, sequence_parallel)
     return x
 
 
