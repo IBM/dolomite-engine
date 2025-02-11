4ef51a57d536437e6bf428eeb617c4fdb1635eaf
Mayank Mishra <mayank31398@gmail.com>
2025-02-11 15:09:03 -0500

    merge
    Signed-off-by: Mayank Mishra <mayank31398@gmail.com>

--- dolomite_engine/hf_models/models/ladder_residual_TP/layer.py
+++ dolomite_engine/hf_models/models/ladder_residual_TP/layer.py
@@ -2,6 +2,7 @@ import torch
 from transformers import DynamicCache
 
 from ..gpt_dolomite_TP.layer import GPTDolomiteBlock_TP
+from ..ladder_residual.layer import LadderResidualBlock
 
 
 class LadderResidualBlock_TP(GPTDolomiteBlock_TP):
@@ -16,29 +17,14 @@ class LadderResidualBlock_TP(GPTDolomiteBlock_TP):
         cu_seqlens: torch.Tensor | None = None,
         max_seqlen: torch.Tensor | None = None,
     ) -> tuple[torch.Tensor]:
-        if current_attention_out is not None:
-            residual = residual + current_attention_out
-
-        current_attention_out = self.ln_1(residual)
-        current_attention_out = self.sequence_mixer(
-            current_attention_out,
+        return LadderResidualBlock.forward(
+            self,
+            current_attention_out=current_attention_out,
+            current_mlp_out=current_mlp_out,
+            residual=residual,
             past_key_values=past_key_values,
             attention_mask=attention_mask,
             rope_cos_sin=rope_cos_sin,
             cu_seqlens=cu_seqlens,
             max_seqlen=max_seqlen,
         )
-
-        if self.m_residual is not None:
-            current_attention_out = current_attention_out * self.m_residual
-
-        if current_mlp_out is not None:
-            residual = residual + current_mlp_out
-
-        current_mlp_out = self.ln_2(residual)
-        current_mlp_out = self.mlp_block(current_mlp_out)
-
-        if self.m_residual is not None:
-            current_mlp_out = current_mlp_out * self.m_residual
-
-        return current_attention_out, current_mlp_out, residual
