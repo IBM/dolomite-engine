d86c5312921e11139a7bd7bfcc7a5fcab61ab3cb
Mayank Mishra <mayank.mishra2@ibm.com>
2025-02-11 04:09:07 +0000

    init

--- dolomite_engine/hf_models/models/ladder_residual_TP/rmsnorm.py
+++ dolomite_engine/hf_models/models/ladder_residual_TP/rmsnorm.py
@@ -9,6 +9,8 @@ from ....distributed import dtensor_to_tensor, tensor_to_dtensor
 from ....kernels import wait_for_ACT
 
 
+_ATTN_F = []
+_ATTN_B = []
 _MLP_F = []
 _MLP_B = []
 
@@ -17,7 +19,7 @@ class _RMSNorm_Cute_F(torch.autograd.Function):
     @staticmethod
     @ensure_contiguous
     def forward(
-        ctx, x: torch.Tensor, weight: torch.Tensor | None, eps: float | None, sequence_parallel: bool
+        ctx, x: torch.Tensor, weight: torch.Tensor | None, eps: float | None, sequence_parallel: bool, context: str
     ) -> torch.Tensor:
         if weight is not None:
             device_mesh = weight.device_mesh
@@ -46,25 +48,28 @@ class _RMSNorm_Cute_F(torch.autograd.Function):
         if is_x_1d:
             output = output.squeeze(0)
 
-        _MLP_F.append((x, weight, rmsnorm_denominator, is_x_1d, eps, sequence_parallel, device_mesh))
+        ctx.context = context
+
+        (_MLP_F if context == "mlp" else _ATTN_F).append((x, weight, rmsnorm_denominator, is_x_1d, eps, sequence_parallel, device_mesh))
 
         return output
 
     @staticmethod
     def backward(ctx, output_grad: torch.Tensor) -> tuple[torch.Tensor | None]:
-        _MLP_B.append(output_grad)
-        return None, None, None, None
+        (_MLP_B if ctx.context == "mlp" else _ATTN_B).append(output_grad)
+        return None, None, None, None, None
 
 
 class _RMSNorm_Cute_B(torch.autograd.Function):
     @staticmethod
-    def forward(ctx, x: torch.Tensor, weight: torch.Tensor | None, eps: float | None) -> torch.Tensor:
+    def forward(ctx, x: torch.Tensor, weight: torch.Tensor | None, eps: float | None, context: str) -> torch.Tensor:
+        ctx.context = context
         return x, weight
 
     @staticmethod
     def backward(ctx, x_grad: torch.Tensor, weight_grad: torch.Tensor) -> tuple[torch.Tensor | None]:
-        x, weight, rmsnorm_denominator, is_x_1d, eps, sequence_parallel, device_mesh = _MLP_F.pop()
-        output_grad = _MLP_B.pop()
+        x, weight, rmsnorm_denominator, is_x_1d, eps, sequence_parallel, device_mesh = (_MLP_F if context == "mlp" else _ATTN_F).pop()
+        output_grad = (_MLP_B if ctx.context == "mlp" else _ATTN_B).pop()
 
         output_grad = output_grad.contiguous()
 
@@ -89,18 +94,18 @@ class _RMSNorm_Cute_B(torch.autograd.Function):
             weight_grad, device_mesh=device_mesh, current_placement=Partial() if sequence_parallel else Replicate()
         )
 
-        return x_grad, weight_grad, None
+        return x_grad, weight_grad, None, None
 
 
 def rmsnorm_cute_forward(
-    x: torch.Tensor, weight: torch.Tensor | None, eps: float | None, sequence_parallel: bool
+    x: torch.Tensor, weight: torch.Tensor | None, eps: float | None, sequence_parallel: bool, context
 ) -> torch.Tensor:
     x = wait_for_ACT(x, wait_in_forward=True, wait_in_backward=False)
-    x = _RMSNorm_Cute_F.apply(x, weight, eps, sequence_parallel)
+    x = _RMSNorm_Cute_F.apply(x, weight, eps, sequence_parallel, context)
     return x
 
 
 def rmsnorm_cute_backward(
-    x: torch.Tensor, weight: torch.Tensor | None, eps: float | None, sequence_parallel: bool
+    x: torch.Tensor, weight: torch.Tensor | None, eps: float | None, sequence_parallel: bool, context
 ) -> torch.Tensor:
-    return _RMSNorm_Cute_B.apply(x, weight, eps)
+    return _RMSNorm_Cute_B.apply(x, weight, eps, context)
