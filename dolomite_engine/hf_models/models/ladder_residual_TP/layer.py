d02259ce0f331229336f9f5d5cfd564cf5a02f4a
Mayank Mishra <mayank31398@gmail.com>
2025-02-10 21:28:39 -0500

    merge
    Signed-off-by: Mayank Mishra <mayank31398@gmail.com>

--- dolomite_engine/hf_models/models/ladder_residual_TP/layer.py
+++ dolomite_engine/hf_models/models/ladder_residual_TP/layer.py
@@ -45,8 +45,10 @@ class LadderResidualBlock_TP(GPTDolomiteBlock_TP):
 
         ln_2_weight = self.ln_2.weight
 
-        # current_attention_out = rmsnorm_cute_wrapper(residual, self.ln_1.weight, self.ln_1.eps, self.sequence_parallel)
-        current_attention_out = rmsnorm_cute_backward(residual, ln_2_weight, self.ln_2.eps, self.sequence_parallel)
+        current_attention_out = rmsnorm_cute_wrapper(residual, self.ln_1.weight, self.ln_1.eps, self.sequence_parallel)
+        current_mlp_out, ln_2_weight = rmsnorm_cute_backward(
+            residual, ln_2_weight, self.ln_2.eps, self.sequence_parallel
+        )
         current_attention_out = self.sequence_mixer(
             current_attention_out,
             past_key_values=past_key_values,
