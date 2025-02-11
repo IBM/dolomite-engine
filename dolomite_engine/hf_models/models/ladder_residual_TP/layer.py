96a0087bb8897fcf988f4c07a666070fa1eebc85
Mayank Mishra <mayank31398@gmail.com>
2025-02-10 20:33:32 -0500

    merge
    Signed-off-by: Mayank Mishra <mayank31398@gmail.com>

--- dolomite_engine/hf_models/models/ladder_residual_TP/layer.py
+++ dolomite_engine/hf_models/models/ladder_residual_TP/layer.py
@@ -2,6 +2,7 @@ import torch
 from cute_kernels import rmsnorm_cute
 from transformers import DynamicCache
 
+from ....kernels import wait_for_ACT
 from ..gpt_dolomite_TP.layer import GPTDolomiteBlock_TP
 from .rmsnorm import rmsnorm_cute_backward_only, rmsnorm_cute_forward_only
 
@@ -21,7 +22,13 @@ class LadderResidualBlock_TP(GPTDolomiteBlock_TP):
         if current_attention_out is not None:
             residual = residual + current_attention_out
 
-        current_attention_out = rmsnorm_cute(x=residual, weight=self.ln_1.weight, eps=self.ln_1.eps)
+        current_attention_out = rmsnorm_cute(
+            x=wait_for_ACT(residual, wait_in_forward=True, wait_in_backward=False),
+            weight=wait_for_ACT(self.ln_1.weight, wait_in_forward=True, wait_in_backward=False),
+            eps=self.ln_1.eps,
+        )
+        current_attention_out = wait_for_ACT(current_attention_out, wait_in_forward=False, wait_in_backward=True)
+
         current_attention_out = self.sequence_mixer(
             current_attention_out,
             past_key_values=past_key_values,
@@ -37,7 +44,13 @@ class LadderResidualBlock_TP(GPTDolomiteBlock_TP):
         if current_mlp_out is not None:
             residual = residual + current_mlp_out
 
-        current_mlp_out = rmsnorm_cute(x=residual, weight=self.ln_2.weight, eps=self.ln_2.eps)
+        current_mlp_out = rmsnorm_cute(
+            x=wait_for_ACT(residual, wait_in_forward=True, wait_in_backward=False),
+            weight=wait_for_ACT(self.ln_2.weight, wait_in_forward=True, wait_in_backward=False),
+            eps=self.ln_1.eps,
+        )
+        current_mlp_out = wait_for_ACT(current_mlp_out, wait_in_forward=False, wait_in_backward=True)
+
         current_mlp_out = self.mlp_block(current_mlp_out)
 
         if self.m_residual is not None:
