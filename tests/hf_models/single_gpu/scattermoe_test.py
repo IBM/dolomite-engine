511970366c1cec7a135fb998d43780ceadc8e9b3
Mayank Mishra <mayank31398@gmail.com>
2025-03-03 23:04:33 -0500

    cleanup
    Signed-off-by: Mayank Mishra <mayank31398@gmail.com>

--- tests/hf_models/single_gpu/scattermoe_test.py
+++ tests/hf_models/single_gpu/scattermoe_test.py
@@ -4,7 +4,7 @@ from transformers import set_seed
 
 from dolomite_engine.hf_models import AttentionHeadType, PositionEmbeddingType
 
-from ...test_common import TestCommons
+from ..test_common import TestCommons
 
 
 SEED = 1234
