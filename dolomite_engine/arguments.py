32079aad38a6205d2ab4f7e184b3b07064bd470a
Mayank Mishra <mayank31398@gmail.com>
2025-03-20 12:44:55 -0400

    cleanup
    Signed-off-by: Mayank Mishra <mayank31398@gmail.com>

--- dolomite_engine/arguments.py
+++ dolomite_engine/arguments.py
@@ -434,8 +434,9 @@ class TrainingArgs(BaseArgs):
             assert self.training_parameters.gradient_accumulation_steps == 1
             assert self.training_parameters.gradient_clipping is None
 
-            log_rank_0(
-                logging.WARNING, "use_optimizer_with_backward_hook doesn't support saving or loading checkpoint"
+            raise NotImplementedError(
+                "use_optimizer_with_backward_hook doesn't support saving or loading checkpoint, comment this "
+                "assertion out to play with this, this is purely experimental"
             )
 
 
