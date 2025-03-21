6c9abfe157caf75e4c295f8e47414ca4890a9be8
Mayank Mishra <mayank31398@gmail.com>
2025-03-21 04:17:20 -0400

    cleanup
    Signed-off-by: Mayank Mishra <mayank31398@gmail.com>

--- dolomite_engine/hf_models/modeling_utils/sequence_mixer_blocks/mla/base.py
+++ dolomite_engine/hf_models/modeling_utils/sequence_mixer_blocks/mla/base.py
@@ -9,18 +9,17 @@ from .....utils import divide_if_divisible
 from ....enums import AttentionHeadType, InitMethod, PositionEmbeddingType
 from ...linear import ParameterizedLinear
 from ...position_embedding import apply_rotary_pos_emb
-from ..softmax_attention import repeat_key_value
 
 
 class MultiHeadLatentAttention(nn.Module):
     def __init__(
         self,
         hidden_size: int,
+        compress_query: bool,
         query_compression_size: int,
         key_value_compression_size: int,
-        key_rope_size: int,
+        rope_dim: int,
         num_attention_heads: int,
-        num_key_value_heads: int,
         attention_multiplier: float,
         attention_head_type: AttentionHeadType,
         position_embedding_type: PositionEmbeddingType,
@@ -39,12 +38,8 @@ class MultiHeadLatentAttention(nn.Module):
         self.causal = causal
         self.hidden_size = hidden_size
         self.num_heads = num_attention_heads
-        self.num_key_value_heads = num_key_value_heads
         self.add_bias = add_bias
-
-        self.query_compression_size = query_compression_size
-        self.key_value_compression_size = key_value_compression_size
-        self.key_rope_size = key_rope_size
+        self.compress_query = compress_query
 
         self.head_dim = divide_if_divisible(
             self.hidden_size,
@@ -52,65 +47,44 @@ class MultiHeadLatentAttention(nn.Module):
             f"`hidden_size` ({self.hidden_size}) must be divisible by `num_heads` ({self.num_heads})",
         )
 
+        self.query_compression_size = query_compression_size
+        self.key_value_compression_size = key_value_compression_size
+        self.rope_dim = rope_dim
+
+        if self.position_embedding_type == PositionEmbeddingType.rope:
+            assert self.rope_dim != 0
+        else:
+            assert self.rope_dim == 0
+
         self.attention_head_type = attention_head_type
         self.position_embedding_type = position_embedding_type
         self.attention_multiplier = attention_multiplier
         self.layer_idx = layer_idx
 
-        if self.attention_head_type == AttentionHeadType.mha:
-            if self.num_key_value_heads is None:
-                self.num_key_value_heads = self.num_heads
-
-            assert (
-                self.num_heads == self.num_key_value_heads
-            ), f"{self.__class__.__name__} should have same number of heads for query, keys and values"
-        elif self.attention_head_type == AttentionHeadType.gqa:
-            assert (
-                self.num_key_value_heads is not None
-            ), "`num_key_value_heads` needs to be specified with GroupedQueryAttention"
-
-            divide_if_divisible(
-                self.num_heads,
-                self.num_key_value_heads,
-                f"`num_heads` ({self.num_heads}) should be a multiple of `num_key_value_heads` ({self.num_key_value_heads})",
-            )
-        elif self.attention_head_type == AttentionHeadType.mqa:
-            if self.num_key_value_heads is None:
-                self.num_key_value_heads = 1
-
-            assert self.num_key_value_heads == 1, f"{self.__class__.__name__} should have 1 head for keys and values"
-        else:
-            raise ValueError(f"unexpected attention_head_type ({self.attention_head_type})")
-
         if init_method == InitMethod.mup:
             raise NotImplementedError("implementation needs to be checked for mup")
 
-        # note that the actual layout is different for the output and depends on whether we are using MHA, MQA or GQA
-        # (self.hidden_size + 2 * self.num_key_value_heads * self.head_dim) is just the actual number output features
         std = initializer_range
         if init_method == InitMethod.mup:
             std /= math.sqrt(m_width)
+
         self.c_attn_down_projection = ParameterizedLinear(
             self.hidden_size,
-            self.query_compression_size
-            + 2 * self.key_value_compression_size
-            + (self.key_rope_size if self.position_embedding_type == PositionEmbeddingType.rope else 0),
+            self.query_compression_size + 2 * self.key_value_compression_size + self.rope_dim,
             bias=self.add_bias,
             std=std,
         )
 
-        # TODO add mup
         self.query_up_projection = ParameterizedLinear(
-            self.query_compression_size,
-            (1 + (self.position_embedding_type == PositionEmbeddingType.rope)) * self.num_heads * self.head_dim,
+            self.hidden_size,
+            self.query_compression_size + 2 * self.key_value_compression_size + self.rope_dim,
             bias=self.add_bias,
             std=std,
         )
 
-        # TODO add mup
         self.key_value_up_projection = ParameterizedLinear(
-            self.key_value_compression_size,
-            self.num_key_value_heads * self.head_dim,
+            self.hidden_size,
+            self.query_compression_size + 2 * self.key_value_compression_size + self.rope_dim,
             bias=self.add_bias,
             std=std,
         )
@@ -124,53 +98,6 @@ class MultiHeadLatentAttention(nn.Module):
 
         self.softmax_dropout = nn.Identity() if softmax_dropout == 0 else nn.Dropout(softmax_dropout)
         self.dropout = nn.Identity() if dropout == 0 else nn.Dropout(dropout)
-
-    def _prepare_qkv_for_forward_mha(
-        self, hidden_states: torch.Tensor
-    ) -> tuple[torch.Tensor, torch.Tensor, torch.Tensor]:
-        batch_size, query_length = hidden_states.shape[:-1]
-
-        hidden_states = hidden_states.view(batch_size, query_length, self.num_heads, -1)
-        hidden_states = hidden_states.transpose(1, 2)
-
-        query, key, value = hidden_states.chunk(3, dim=-1)
-
-        return query, key, value
-
-    def _prepare_qkv_for_forward_gqa(
-        self, hidden_states: torch.Tensor
-    ) -> tuple[torch.Tensor, torch.Tensor, torch.Tensor]:
-        batch_size, query_length = hidden_states.shape[:-1]
-
-        hidden_states = hidden_states.view(batch_size, query_length, self.num_key_value_heads, -1)
-
-        query, key, value = hidden_states.split(
-            ((self.num_heads // self.num_key_value_heads) * self.head_dim, self.head_dim, self.head_dim), dim=-1
-        )
-
-        # this needs to be a reshape instead of view sadly
-        query = query.reshape(batch_size, query_length, -1, self.head_dim)
-
-        query = query.transpose(1, 2)
-        key = key.transpose(1, 2)
-        value = value.transpose(1, 2)
-
-        return query, key, value
-
-    def _prepare_qkv_for_forward_mqa(
-        self, hidden_states: torch.Tensor
-    ) -> tuple[torch.Tensor, torch.Tensor, torch.Tensor]:
-        batch_size, query_length = hidden_states.shape[:-1]
-
-        query, key, value = hidden_states.split((self.hidden_size, self.head_dim, self.head_dim), dim=-1)
-
-        query = query.view(batch_size, query_length, self.num_heads, -1)
-
-        query = query.transpose(1, 2)
-        key = key.unsqueeze(1)
-        value = value.unsqueeze(1)
-
-        return query, key, value
 
     def forward(
         self,
@@ -190,62 +117,69 @@ class MultiHeadLatentAttention(nn.Module):
 
         if self.position_embedding_type == PositionEmbeddingType.rope:
             compressed_query, compressed_key_value, key_rope = hidden_states.split(
-                (self.query_compression_size, 2 * self.key_value_compression_size, self.key_rope_size), dim=-1
-            )
-            key_rope = apply_rotary_pos_emb(key_rope, rope_cos_sin)
-        else:
-            compressed_query, compressed_key_value = hidden_states.split(
-                (self.query_compression_size, 2 * self.key_value_compression_size), dim=-1
+                (
+                    self.query_compression_size,
+                    2 * self.key_value_compression_size,
+                    self.rope_dim,
+                ),
+                dim=-1,
             )
 
-        query = self.query_up_projection(compressed_query)
-
-        # TODO write a fused kernel for this to avoid copy
-        if self.position_embedding_type == PositionEmbeddingType.rope:
-            query, query_rope = query.chunk(2, dim=-1)
+            query, query_rope = self.query_up_projection(compressed_query)
             query_rope = apply_rotary_pos_emb(query_rope, rope_cos_sin)
             query = torch.cat([query, query_rope], dim=-1)
             del query_rope
 
             key_rope = apply_rotary_pos_emb(key_rope, rope_cos_sin)
-            key = 0
 
-        if past_key_values is not None:
-            compressed_key_value = past_key_values.update(compressed_key_value, key_rope, self.layer_idx)
+            if past_key_values is not None:
+                compressed_key_value, key_rope = past_key_values.update(compressed_key_value, key_rope, self.layer_idx)
 
-        key_value = self.key_value_up_projection(compressed_key_value)
+            key_value = self.key_value_up_projection(compressed_key_value)
+            key, value = key_value.chunk(2, dim=-1)
+            del key_value
 
-        key, value = key_value.chunk(2, dim=-1)
-
-        # ==========================================================================================
-        # hidden_states -> (batch_size, query_length, [num_heads + num_key_value_heads * 2] * head_dim)
-        # ==========================================================================================
-
-        # for MHA, we can get away with doing just 1 transpose which is not true for GQA
-        if self.attention_head_type == AttentionHeadType.mha:
-            query, key, value = self._prepare_qkv_for_forward_mha(hidden_states)
-        elif self.attention_head_type == AttentionHeadType.gqa:
-            query, key, value = self._prepare_qkv_for_forward_gqa(hidden_states)
-        elif self.attention_head_type == AttentionHeadType.mqa:
-            query, key, value = self._prepare_qkv_for_forward_mqa(hidden_states)
+            key = torch.cat([key, key_rope], dim=-1)
+            del key_rope
         else:
-            raise ValueError(f"unexpected attention_head_type ({self.attention_head_type})")
+            compressed_query, compressed_key_value, key_rope = hidden_states.split(
+                (
+                    self.query_compression_size,
+                    2 * self.key_value_compression_size,
+                    self.rope_dim,
+                ),
+                dim=-1,
+            )
+
+            query, query_rope = self.query_up_projection(compressed_query)
+            query_rope = apply_rotary_pos_emb(query_rope, rope_cos_sin)
+            query = torch.cat([query, query_rope], dim=-1)
+            del query_rope
+
+            key_rope = apply_rotary_pos_emb(key_rope, rope_cos_sin)
+
+            if past_key_values is not None:
+                compressed_key_value, key_rope = past_key_values.update(compressed_key_value, key_rope, self.layer_idx)
+
+            key_value = self.key_value_up_projection(compressed_key_value)
+            key, value = key_value.chunk(2, dim=-1)
+            del key_value
+
+            key = torch.cat([key, key_rope], dim=-1)
+            del key_rope
 
         key = key.transpose(-1, -2)
         dtype = query.dtype
 
         # ==========================================================================================
         # query -> (batch_size, num_heads, query_length, head_dim)
-        # key -> (batch_size, num_key_value_heads, head_dim, key_length)
-        # value -> (batch_size, num_key_value_heads, key_length, head_dim)
+        # key -> (batch_size, num_heads, head_dim, key_length)
+        # value -> (batch_size, num_heads, key_length, head_dim)
         # ==========================================================================================
 
         batch_size = query.shape[0]
         query_length = query.shape[2]
         key_length = key.shape[-1]
-
-        key = repeat_key_value(key, self.num_heads, self.num_key_value_heads)
-        value = repeat_key_value(value, self.num_heads, self.num_key_value_heads)
 
         # Always copies
         query = query.reshape(batch_size * self.num_heads, query_length, self.head_dim)
