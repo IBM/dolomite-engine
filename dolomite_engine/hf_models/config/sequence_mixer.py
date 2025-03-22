dd27af93d4478c8115beaf693f1be13aef18eb52
Mayank Mishra <mayank31398@gmail.com>
2025-03-22 16:51:48 -0400

    cleanup
    Signed-off-by: Mayank Mishra <mayank31398@gmail.com>

--- dolomite_engine/hf_models/config/sequence_mixer.py
+++ dolomite_engine/hf_models/config/sequence_mixer.py
@@ -19,8 +19,6 @@ class _SoftmaxAttentionArgs(BaseArgs):
 
 class _MultiHeadLatentAttention(BaseArgs):
     sequence_mixer_type: str = "multihead_latent_attention"
-    num_key_value_heads: int = 1
-    attention_head_type: AttentionHeadType = AttentionHeadType.mqa
     softmax_dropout: float = 0
     dropout: float = 0
     add_bias: bool = True
