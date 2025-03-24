2900ab777ee71768d786b396f4e1292e3d4028dd
Mayank Mishra <mayank31398@gmail.com>
2025-03-24 18:50:20 -0400

    cleanup
    Signed-off-by: Mayank Mishra <mayank31398@gmail.com>

--- dolomite_engine/hf_models/mixins/modeling_outputs.py
+++ dolomite_engine/hf_models/mixins/modeling_outputs.py
@@ -13,7 +13,7 @@ class BaseModelOutputWithPast(ModelOutput):
 @dataclass
 class CausalLMOutputWithPast(ModelOutput):
     loss: torch.Tensor | None = None
-    aux_loss: torch.Tensor | float | None = None
+    aux_loss: torch.Tensor | float = 0
     logits: torch.Tensor | None = None
     past_key_values: tuple[tuple[torch.Tensor]] | None = None
     last_hidden_state: torch.Tensor | None = None
