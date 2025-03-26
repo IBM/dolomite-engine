ba888921cd2b048c7b0150cff4fd97b6c28a2531
Mayank Mishra <mayank31398@gmail.com>
2025-03-26 00:00:23 -0400

    cleanup
    Signed-off-by: Mayank Mishra <mayank31398@gmail.com>

--- dolomite_engine/train_utils.py
+++ dolomite_engine/train_utils.py
@@ -89,11 +89,11 @@ def get_model_tflops(
         sequence_mixer_type = block.sequence_mixer_type
 
         if sequence_mixer_type in ["softmax_attention", "stickbreaking_attention"]:
+            attention_flops = 4 * b * s * h * (h * (1 + block.num_key_value_heads / n) + s)
+        elif sequence_mixer_type == "multihead_latent_attention":
             attention_flops = (
                 2 * b * s * h * (h + 2 * (s + block.query_compression_size + 2 * block.key_value_compression_size))
             )
-        elif sequence_mixer_type == "multihead_latent_attention":
-            attention_flops = 4 * b * s * h * (h * (1 + block.num_key_value_heads / n) + s)
         elif sequence_mixer_type == "mamba2":
             # NOTE taken from NexaAI's fork
             # Mamba2 FLOP calculation based on its specific architecture
