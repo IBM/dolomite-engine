dfab63f1ecd913e0f1b6321978f0112a7ba70f9b
Mayank Mishra <mayank31398@gmail.com>
2025-04-13 18:32:19 -0400

    op
    Signed-off-by: Mayank Mishra <mayank31398@gmail.com>

--- dolomite_engine/hf_models/modeling_utils_TP/sequence_mixer_blocks/__init__.py
+++ dolomite_engine/hf_models/modeling_utils_TP/sequence_mixer_blocks/__init__.py
@@ -14,10 +14,9 @@ def get_sequence_mixer_TP(
 
     sequence_mixer_kwargs = dict(
         hidden_size=config.hidden_size,
-        num_attention_heads=config.num_attention_heads,
+        num_attention_heads=block.num_attention_heads,
         num_key_value_heads=block.num_key_value_heads,
         attention_multiplier=block.attention_multiplier,
-        attention_head_type=block.attention_head_type,
         position_embedding_type=config.position_embedding_type,
         add_bias=block.add_bias,
         softmax_dropout=block.softmax_dropout,
