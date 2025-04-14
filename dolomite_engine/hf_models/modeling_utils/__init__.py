4b6e3e6e5a67acf84064d578768ebcbb7b575019
Mayank Mishra <mayank31398@gmail.com>
2025-04-14 14:26:37 -0400

    op
    Signed-off-by: Mayank Mishra <mayank31398@gmail.com>

--- dolomite_engine/hf_models/modeling_utils/__init__.py
+++ dolomite_engine/hf_models/modeling_utils/__init__.py
@@ -13,6 +13,7 @@ from .mlp_blocks import (
 from .normalization import get_normalization_function
 from .position_embedding import RoPE, YaRNScaledRoPE, apply_rotary_pos_emb
 from .sequence_mixer_blocks import (
+    RNN,
     Attention,
     Mamba2,
     get_attention_head_type,
