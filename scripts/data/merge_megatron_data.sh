2a7fc09ba3ddd5c3236baad1bc76d7da8411f674
Mayank Mishra <mayank31398@gmail.com>
2025-04-23 18:28:39 -0400

    cleanup
    Signed-off-by: Mayank Mishra <mayank31398@gmail.com>

---
+++ scripts/data/merge_megatron_data.sh
@@ -0,0 +1,7 @@
+python tools/data/convert_fms_data_to_megatron.py \
+    --input-path $INPUT_PATH \
+    --dataset-list $DATASET_LIST \
+    --tmp-path $TMP_PATH \
+    --output-path $OUTPUT_PATH \
+    --tokenizer-path $TOKENIZER_PATH \
+    --merge
