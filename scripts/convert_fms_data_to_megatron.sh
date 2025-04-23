123de0eeba3bd287da6165a3bef0c99488dbb754
Mayank Mishra <mayank31398@gmail.com>
2025-04-23 18:24:48 -0400

    cleanup
    Signed-off-by: Mayank Mishra <mayank31398@gmail.com>

--- scripts/convert_fms_data_to_megatron.sh
+++ scripts/convert_fms_data_to_megatron.sh
@@ -1,16 +1,14 @@
-TOKENIZER_PATH=tokenizers/gpt-neox-20b
-
-DATASET_LIST=sampling_proportions/bluepile-03-granite/proportions.txt
-
+TOKENIZER_PATH=bigcode/starcoder
 INPUT_PATH=/dataset/bluepile
 TMP_PATH=/dataset/bluepile/tmp
 OUTPUT_PATH=/dataset/bluepile/megatron
 
-python tools/convert_fms_data_to_megatron.py \
+python tools/data/convert_fms_data_to_megatron.py \
     --input-path $INPUT_PATH \
-    --dataset-list $DATASET_LIST \
+    --data-subsets _tokenization2arrow \
     --tmp-path $TMP_PATH \
     --output-path $OUTPUT_PATH \
-    --tokenizer-path $TOKENIZER_PATH \
+    --tokenizer $TOKENIZER_PATH \
     --convert \
-    --ccc-job
+    --num-files-per-job 200 \
+    --blue-vela-job
