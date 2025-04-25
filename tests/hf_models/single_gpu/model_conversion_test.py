e5745525c765d3a0196e169cf295e48d625662cc
Sukriti Sharma <Ssukriti@users.noreply.github.com>
2025-04-25 15:19:50 -0600

    Granitemoe hybrid cleanup (#184)
    Signed-off-by: Sukriti-Sharma4 <sukriti.sharma4@ibm.com>
    Signed-off-by: Mayank Mishra <mayank31398@gmail.com>
    Co-authored-by: Mayank Mishra <mayank31398@gmail.com>

--- tests/hf_models/single_gpu/model_conversion_test.py
+++ tests/hf_models/single_gpu/model_conversion_test.py
@@ -1,6 +1,8 @@
 import torch
 from parameterized import parameterized
 
+from dolomite_engine.hf_models.config import _Mamba2Args
+
 from ..test_common import TestCommons
 
 
@@ -95,3 +97,37 @@ class ModelConversionTest(TestCommons):
             exact_match=False,
             compare_loss=False,
         )
+
+    @parameterized.expand(
+        TestCommons.make_args_matrix(TestCommons.get_all_devices(), TestCommons.get_attention_head_types())
+    )
+    def test_granitemoehybrid_model_conversion(self, device: torch.device, attention_head_type: str) -> None:
+        # TODO remove try except once GraniteMoeHybrid is added into HF
+        try:
+            from transformers import GraniteMoeHybridConfig, GraniteMoeHybridForCausalLM
+
+            dolomite_config = self.get_moe_test_config(
+                attention_head_type,
+                "nope",
+                add_bias=False,
+                shared_n_inner=64,
+                activation_function="swiglu",
+                normalization_function="rmsnorm",
+                m_emb=2,
+                m_width=2,
+            )
+
+            for layer in range(dolomite_config.num_layers):
+                if layer % 2 == 0:
+                    dolomite_config.sequence_mixer_blocks[layer] = _Mamba2Args(intermediate_size=256)
+
+            self.model_conversion_test(
+                dolomite_config=dolomite_config,
+                model_type="granitemoehybrid",
+                device=device,
+                exact_match=False,
+                compare_loss=False,
+                logits_atol_float32=2.5e-5,
+            )
+        except:
+            pass
