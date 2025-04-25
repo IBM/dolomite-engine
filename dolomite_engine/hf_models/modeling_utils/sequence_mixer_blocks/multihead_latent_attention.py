f7f2c2dec93f52c49cf5acf317e3209ed1de687b
Mayank Mishra <32954280+mayank31398@users.noreply.github.com>
2025-04-25 17:25:23 -0400

    support expansion with MLA (#166)
    Signed-off-by: Mayank Mishra <mayank31398@gmail.com>

--- dolomite_engine/hf_models/modeling_utils/sequence_mixer_blocks/multihead_latent_attention.py
+++ dolomite_engine/hf_models/modeling_utils/sequence_mixer_blocks/multihead_latent_attention.py
@@ -8,8 +8,9 @@ from transformers.modeling_flash_attention_utils import _flash_attention_forward
 
 from ....enums import Kernel
 from ....kernels import is_kernel_allowed, wait_for_ACT
-from ....utils import divide_if_divisible, is_flash_attention_available
+from ....utils import is_flash_attention_available
 from ..linear import ParameterizedLinear
+from ..normalization import get_normalization_function
 
 
 if is_flash_attention_available():
@@ -23,6 +24,7 @@ class MultiHeadLatentAttention(nn.Module):
         query_compression_size: int,
         key_value_compression_size: int,
         num_attention_heads: int,
+        head_dim: int,
         attention_multiplier: float,
         position_embedding_type: str,
         add_bias: bool,
@@ -35,23 +37,19 @@ class MultiHeadLatentAttention(nn.Module):
         causal: bool,
         layer_idx: int,
         use_padding_free_transformer: bool,
+        normalization_function: str,
+        layer_norm_epsilon: float = 1e-5,
     ) -> None:
         super().__init__()
 
         self.causal = causal
         self.hidden_size = hidden_size
         self.num_heads = num_attention_heads
+        self.head_dim = head_dim
         self.add_bias = add_bias
         self.use_padding_free_transformer = use_padding_free_transformer
         self.query_compression_size = query_compression_size
         self.key_value_compression_size = key_value_compression_size
-
-        self.head_dim = divide_if_divisible(
-            self.hidden_size,
-            self.num_heads,
-            f"`hidden_size` ({self.hidden_size}) must be divisible by `num_heads` ({self.num_heads})",
-        )
-
         self.position_embedding_type = position_embedding_type
         self.attention_multiplier = attention_multiplier
         self.layer_idx = layer_idx
@@ -63,29 +61,43 @@ class MultiHeadLatentAttention(nn.Module):
         if self.position_embedding_type == "rope":
             raise NotImplementedError()
         else:
-            self.c_attn_down_projection = ParameterizedLinear(
+            self.query_down_projection = ParameterizedLinear(
+                self.hidden_size, self.query_compression_size, bias=self.add_bias, std=std
+            )
+
+            self.query_ln = get_normalization_function(
+                normalization_function, self.query_compression_size, eps=layer_norm_epsilon
+            )
+
+            self.query_up_projection = ParameterizedLinear(
+                self.query_compression_size, self.num_heads * self.head_dim, bias=self.add_bias, std=std
+            )
+
+            self.key_value_down_projection = ParameterizedLinear(
                 self.hidden_size,
-                self.query_compression_size + 2 * self.key_value_compression_size,
+                2 * self.key_value_compression_size,
                 bias=self.add_bias,
                 std=std,
             )
 
-            self.query_up_projection = ParameterizedLinear(
-                self.query_compression_size, self.hidden_size, bias=self.add_bias, std=std
+            self.key_value_ln = get_normalization_function(
+                normalization_function, 2 * self.key_value_compression_size, eps=layer_norm_epsilon
             )
 
             self.key_up_projection = ParameterizedLinear(
-                self.key_value_compression_size, self.hidden_size, bias=self.add_bias, std=std
+                self.key_value_compression_size, self.num_heads * self.head_dim, bias=self.add_bias, std=std
             )
 
             self.value_up_projection = ParameterizedLinear(
-                self.key_value_compression_size, self.hidden_size, bias=self.add_bias, std=std
+                self.key_value_compression_size, self.num_heads * self.head_dim, bias=self.add_bias, std=std
             )
 
         std = initializer_range / math.sqrt(2 * num_layers)
         if init_method == "mup":
             std /= math.sqrt(m_width)
-        self.c_proj = ParameterizedLinear(self.hidden_size, self.hidden_size, bias=self.add_bias, std=std)
+        self.c_proj = ParameterizedLinear(
+            self.num_heads * self.head_dim, self.hidden_size, bias=self.add_bias, std=std
+        )
 
         self.softmax_dropout_p = softmax_dropout
 
@@ -105,16 +117,18 @@ class MultiHeadLatentAttention(nn.Module):
             assert is_kernel_allowed(Kernel.flash_attention_2)
             assert past_key_values is None
 
-        hidden_states = self.c_attn_down_projection(hidden_states)
+        query = self.query_down_projection(hidden_states)
+        query = self.query_ln(query)
+
+        key_value = self.key_value_down_projection(hidden_states)
+        key_value = self.key_value_ln(key_value)
+        key, value = key_value.chunk(2, dim=-1)
+
+        del hidden_states, key_value
 
         if self.position_embedding_type == "rope":
             raise NotImplementedError()
         else:
-            query, key, value = hidden_states.split(
-                (self.query_compression_size, self.key_value_compression_size, self.key_value_compression_size), dim=-1
-            )
-            del hidden_states
-
             if past_key_values is not None:
                 key, value = past_key_values.update(key.unsqueeze(1), value.unsqueeze(1), self.layer_idx)
                 key = key.squeeze(1)
