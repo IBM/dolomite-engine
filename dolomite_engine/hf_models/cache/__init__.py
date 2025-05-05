a5e789237c5934b8ac9f5d8ecaa06d0b8e4f5bd0
Mayank Mishra <mayank31398@gmail.com>
2025-05-05 03:39:06 -0400

    cleanup
    Signed-off-by: Mayank Mishra <mayank31398@gmail.com>

--- dolomite_engine/hf_models/cache/__init__.py
+++ dolomite_engine/hf_models/cache/__init__.py
@@ -1,11 +1,54 @@
 import torch
-from transformers import DynamicCache
+from transformers import Cache, DynamicCache
 from transformers.models.jamba.modeling_jamba import (
     HybridMambaAttentionDynamicCache as _HybridMambaAttentionDynamicCache,
 )
 
 from ...utils import divide_if_divisible
 from ..config import CommonConfig
+from .attention import SoftmaxAttentionCache
+
+
+class GenerationCache(Cache):
+    def __init__(self) -> None:
+        super().__init__()
+        self._seen_tokens = 0
+        self.cache: list[SoftmaxAttentionCache] = []
+
+    def __getitem__(self, layer_idx: int) -> tuple[torch.Tensor]:
+        if layer_idx < len(self):
+            return self.cache[layer_idx].get_cache()
+
+        raise KeyError(f"Cache only has {len(self)} layers, attempted to access layer with index {layer_idx}")
+
+    def __iter__(self):
+        for layer_idx in range(len(self)):
+            yield self.cache[layer_idx].get_cache()
+
+    def __len__(self):
+        return self.seen_tokens
+
+    def update(self, layer_idx: int, **kwargs) -> tuple[torch.Tensor]:
+        if len(self.key_cache) <= layer_idx:
+            # There may be skipped layers, fill them with empty lists
+            for _ in range(len(self.key_cache), layer_idx):
+                self.key_cache.append(torch.tensor([]))
+                self.value_cache.append(torch.tensor([]))
+            self.key_cache.append(key_states)
+            self.value_cache.append(value_states)
+        elif not self.key_cache[
+            layer_idx
+        ].numel():  # prefers not t.numel() to len(t) == 0 to export the model  # fills previously skipped layers; checking for tensor causes errors
+            self.key_cache[layer_idx] = key_states
+            self.value_cache[layer_idx] = value_states
+        else:
+            self.key_cache[layer_idx] = torch.cat([self.key_cache[layer_idx], key_states], dim=-2)
+            self.value_cache[layer_idx] = torch.cat([self.value_cache[layer_idx], value_states], dim=-2)
+
+        return self.cache[layer_idx].update(**kwargs)
+
+    def get_seq_length(self, layer_idx: int | None = 0) -> int:
+        return self.cache[layer_idx].get_seq_length()
 
 
 class HybridMambaAttentionDynamicCache(_HybridMambaAttentionDynamicCache):
