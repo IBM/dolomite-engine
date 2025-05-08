39e36bc4d5455342b64448b8f9dc2312248fcf93
Mayank Mishra <mayank31398@gmail.com>
2025-05-08 11:54:45 -0400

    cleanup
    Signed-off-by: Mayank Mishra <mayank31398@gmail.com>

--- scripts/coreweave/interactive.sh
+++ scripts/coreweave/interactive.sh
@@ -1,4 +1,4 @@
 CONTAINER=/mnt/vast/squash/ete-dolomite.sqsh
 MOUNT=/mnt:/mnt
 
-srun -N1  --container-image=$CONTAINER  --container-mounts=$MOUNT --no-container-remap-root --pty bash
+srun -N1 --container-image=$CONTAINER --container-mounts=$MOUNT --no-container-remap-root --pty bash
