1ab035a3b27de2370715db5de30ba0025f95c9d6
Mayank Mishra <mayank31398@gmail.com>
2025-05-23 19:18:42 -0400

    cleanup
    Signed-off-by: Mayank Mishra <mayank31398@gmail.com>

--- dolomite_engine/hf_models/modeling_utils/sequence_mixer_blocks/causal_convolution.py
+++ dolomite_engine/hf_models/modeling_utils/sequence_mixer_blocks/causal_convolution.py
@@ -7,13 +7,19 @@ import math
 import torch
 import torch.nn as nn
 
-from ....utils import divide_if_divisible
+from ....enums import Kernel
+from ....kernels import is_kernel_allowed
+from ....utils import divide_if_divisible, is_causal_conv1d_available
 from ...cache import GenerationCache
 from ...parameter import mark_parameter_as_mup_learning_rate, mark_parameter_as_no_weight_decay
 from ..activations import get_activation_function, is_glu
 from ..convolution import ParameterizedConv1d
 from ..linear import ParameterizedLinear
 from ..mlp_blocks.mlp import _get_std_for_linear
+
+
+if is_causal_conv1d_available():
+    from causal_conv1d import causal_conv1d_fn
 
 
 class CausalConvolution(nn.Module):
@@ -38,7 +44,10 @@ class CausalConvolution(nn.Module):
         if use_padding_free_transformer:
             raise NotImplementedError()
 
+        self.in_channels = in_channels
+        self.out_channels = out_channels
         self.kernel_size = kernel_size
+        self.num_groups = num_groups
         self.layer_idx = layer_idx
 
         std = _get_std_for_linear(initializer_range, init_method, m_width)
@@ -68,6 +77,10 @@ class CausalConvolution(nn.Module):
             intermediate_size, hidden_size, bias=add_bias, std=std / math.sqrt(2 * num_layers)
         )
 
+        self.casual_conv1d_compatible = (
+            self.num_groups == self.in_channels == self.out_channels and activation_function in [None, "silu", "swish"]
+        )
+
     def forward(
         self,
         hidden_states: torch.Tensor,
@@ -79,13 +92,23 @@ class CausalConvolution(nn.Module):
             input_state = input_state.roll(shifts=-1, dims=-1)
 
         hidden_states = self.input_projection(hidden_states)
-
         hidden_states = hidden_states.transpose(-1, -2)
-        hidden_states = self.conv1d(hidden_states)
-        hidden_states = hidden_states[..., : -(self.kernel_size - 1)]
 
-        hidden_states = hidden_states.transpose(-1, -2)
-        hidden_states = self.activation_function(hidden_states)
+        if is_kernel_allowed(Kernel.causal_conv1d) and self.casual_conv1d_compatible:
+            hidden_states = causal_conv1d_fn(
+                x=hidden_states,
+                weight=self.conv1d.weight.squeeze(1),
+                bias=self.conv1d.bias,
+                activation=self.activation_string,
+            )
+
+            hidden_states = hidden_states.transpose(1, 2)
+        else:
+            hidden_states = self.conv1d(hidden_states)
+            hidden_states = hidden_states[..., : -(self.kernel_size - 1)]
+
+            hidden_states = hidden_states.transpose(-1, -2)
+            hidden_states = self.activation_function(hidden_states)
 
         hidden_states = self.output_projection(hidden_states)
 
