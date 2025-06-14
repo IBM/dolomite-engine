d602e3b69c31111aa67bdce9b9ccfd452cffcc1e
Mayank Mishra <mayank31398@gmail.com>
2025-06-14 16:07:57 +0300

    cleanup
    Signed-off-by: Mayank Mishra <mayank31398@gmail.com>

--- lm_engine/data/megatron/sampler.py
+++ lm_engine/data/megatron/sampler.py
@@ -1,8 +1,3 @@
-# **************************************************
-# Copyright (c) 2025, Mayank Mishra
-# **************************************************
-
-
 class MegatronBatchSampler:
     def __init__(
         self,
